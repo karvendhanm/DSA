c89750e4c098bd0bb4b19fd8443690752cfe64f8
Karvendhan M <karvsmech@gmail.com>
2022-06-15 20:58:12 +0530

    using reduce function to remove unwanted items from an array

--- functional_programming/convert_string_to_url_slugs.js
+++ functional_programming/convert_string_to_url_slugs.js
@@ -1,17 +1,32 @@
-// Only change code below this line
 function urlSlug(title) {
 
     const pattern = /\s{2,}/g;
     let url = title.trim();
-    url = url.replace(pattern, " ");
     url = url.toLowerCase();
+    url = url.replace(pattern, " ");
     url = url.split(" ");
     url =  url.join("-");
     return url
 
 }
-// Only change code above this line
-console.log(urlSlug("Winter Is Coming"));
-console.log(urlSlug(" Winter Is  Coming"));
-console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
-console.log(urlSlug("Hold The Door"));
+
+
+function _urlSlug(title) {
+
+    let url = title.trim();
+    url = url.toLowerCase();
+    url = url.split(" ");
+    url = url.reduce(function(_lst, item) {
+        item != "" ? _lst.push(item) : 0
+        return _lst;
+    }, []);
+    url =  url.join("-");
+    return url
+
+}
+
+
+console.log(_urlSlug(" Winter Is  Coming"));
+console.log(_urlSlug("Winter Is Coming"));
+console.log(_urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
+console.log(_urlSlug("Hold The Door"));
