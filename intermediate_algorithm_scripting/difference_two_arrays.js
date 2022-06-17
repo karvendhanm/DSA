fc9ccdf3fb0b0f5a528fface5049eb9a440b917a
Karvendhan M <karvsmech@gmail.com>
2022-06-17 22:08:18 +0530

    difference between 2 arrays

---
+++ intermediate_algorithm_scripting/difference_two_arrays.js
@@ -0,0 +1,15 @@
+function diffArray(arr1, arr2) {
+
+    const arr1_set = new Set(arr1);
+    const arr2_set = new Set(arr2);
+
+    arr1_set.
+
+    return newArr;
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+
+
+
