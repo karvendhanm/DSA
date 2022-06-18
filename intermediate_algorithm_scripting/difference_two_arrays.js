21754ee58ff363385a9323c9987f3fcdff7cce06
Karvendhan M <karvsmech@gmail.com>
2022-06-18 21:07:09 +0530

    Symmetric difference between two arrays, 4th attempt

--- intermediate_algorithm_scripting/difference_two_arrays.js
+++ intermediate_algorithm_scripting/difference_two_arrays.js
@@ -1,18 +1,34 @@
+function diff(arr1, arr2) {
+    return arr1.filter(item => !(arr2.includes(item)));
+}
+
 function diffArray(arr1, arr2) {
-
-    const arr = arr1.concat(arr2);
-
-    const newArr = [];
-    arr.filter(item => {
-        if(!(arr1.includes(item)) || !(arr2.includes(item))){
-            newArr.push(item);
-        }
-    })
-
-    return newArr;
+    return[...diff(arr1, arr2), ...diff(arr2, arr1)];
 }
 
 console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+// Symmetric difference between two arrays, 3rd attempt
+// function diffArray(arr1, arr2) {
+//
+//     return arr1.concat(arr2).filter(item => !(arr1.includes(item)) || !(arr2.includes(item)))
+//
+// }
+
+// Symmetric difference between two arrays, 2nd attempt
+// function diffArray(arr1, arr2) {
+//
+//     const arr = arr1.concat(arr2);
+//
+//     const newArr = [];
+//     arr.filter(item => {
+//         if(!(arr1.includes(item)) || !(arr2.includes(item))){
+//             newArr.push(item);
+//         }
+//     })
+//
+//     return newArr;
+// }
 
 // Symmetric difference between two arrays, 1st attempt
 // function diffArray(arr1, arr2) {
