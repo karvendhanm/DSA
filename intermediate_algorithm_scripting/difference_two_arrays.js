2b457da2aa3da93c250db9256cb9c63ce563db9e
Karvendhan M <karvsmech@gmail.com>
2022-06-18 20:48:14 +0530

    symmetric difference of the two arrays 1st attempt

--- intermediate_algorithm_scripting/difference_two_arrays.js
+++ intermediate_algorithm_scripting/difference_two_arrays.js
@@ -2,23 +2,38 @@ function diffArray(arr1, arr2) {
 
     const arr = arr1.concat(arr2);
 
-    let counter = {};
-    arr.forEach(item => {
-        counter[item] = (counter[item] || 0) +  1;
-        return counter;
-    });
-
     const newArr = [];
-    for (let key in counter){
-        if (counter[key] === 1) {
-            newArr.push(parseInt(key) || key);
+    arr.filter(item => {
+        if(!(arr1.includes(item)) || !(arr2.includes(item))){
+            newArr.push(item);
         }
-    }
+    })
 
     return newArr;
 }
 
 console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+// Symmetric difference between two arrays, 1st attempt
+// function diffArray(arr1, arr2) {
+//
+//     const arr = arr1.concat(arr2);
+//
+//     let counter = {};
+//     arr.forEach(item => {
+//         counter[item] = (counter[item] || 0) +  1;
+//         return counter;
+//     });
+//
+//     const newArr = [];
+//     for (let key in counter){
+//         if (counter[key] === 1) {
+//             newArr.push(parseInt(key) || key);
+//         }
+//     }
+//
+//     return newArr;
+// }
 
 
 
