c8a1245b5e708c0c649f55db387aa47559f3b5a1
Karvendhan M <karvsmech@gmail.com>
2022-06-19 21:20:29 +0530

    sek and destroy, 2nd attempt

--- intermediate_algorithm_scripting/seek_and_destroy.js
+++ intermediate_algorithm_scripting/seek_and_destroy.js
@@ -10,16 +10,56 @@ function destroyer(arr) {
         }
     }
 
-    for(let i=0; i<_lst.length; i++){
-        let _idx = arr.indexOf(_lst[i]);
-        while(_idx != -1) {
-            arr.splice(_idx, 1);
-            _idx = arr.indexOf(_lst[i]);
+    arr = arr.filter(item => {
+        if(_lst.indexOf(item) != -1) {
+            return false;
+        } else {
+            return true;
         }
-    }
+    })
 
     return arr;
-
 }
 
 console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+
+// 2nd attempt
+// function destroyer(arr) {
+//     const _lst = [];
+//     for (let key in arguments){
+//         if(key != 0){
+//             _lst.push(arguments[key])
+//         }
+//     }
+//
+//     arr = arr.filter(item => {
+//         if(_lst.indexOf(item) != -1) {
+//             return false;
+//         } else {
+//             return true;
+//         }
+//     })
+//
+//     return arr;
+// }
+
+//  1st attempt
+// function destroyer(arr) {
+//     const _lst = [];
+//     for (let key in arguments){
+//         if(key != 0){
+//             _lst.push(arguments[key])
+//         }
+//     }
+//
+//     for(let i=0; i<_lst.length; i++){
+//         let _idx = arr.indexOf(_lst[i]);
+//         while(_idx != -1) {
+//             arr.splice(_idx, 1);
+//             _idx = arr.indexOf(_lst[i]);
+//         }
+//     }
+//
+//     return arr;
+//
+// }
