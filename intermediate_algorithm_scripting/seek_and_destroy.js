fe4c1e62de2b3c5e8f0bb174d8ee78ba967d45e3
Karvendhan M <karvsmech@gmail.com>
2022-06-19 21:04:38 +0530

    seek and destroy, 1st attempt

--- intermediate_algorithm_scripting/seek_and_destroy.js
+++ intermediate_algorithm_scripting/seek_and_destroy.js
@@ -1,5 +1,25 @@
+// You will be provided with an initial array (the first argument in the destroyer function),
+// followed by one or more arguments. Remove all elements from the initial array that are of the same
+// value as these arguments.
+
 function destroyer(arr) {
-    console.log(arr, arguments);
+    const _lst = [];
+    for (let key in arguments){
+        if(key != 0){
+            _lst.push(arguments[key])
+        }
+    }
+
+    for(let i=0; i<_lst.length; i++){
+        let _idx = arr.indexOf(_lst[i]);
+        while(_idx != -1) {
+            arr.splice(_idx, 1);
+            _idx = arr.indexOf(_lst[i]);
+        }
+    }
+
+    return arr;
+
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
