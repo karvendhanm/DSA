8db5cb953a7d221311af89a6752744619f738b11
Karvendhan M <karvsmech@gmail.com>
2022-06-21 21:36:24 +0530

    wherefore art thou, 2nd attempt

--- intermediate_algorithm_scripting/wherefore_art_thou.js
+++ intermediate_algorithm_scripting/wherefore_art_thou.js
@@ -1,22 +1,37 @@
-// 1st attempt
+// 2nd attempt
 function whatIsInAName(collection, source) {
-    const arr = [];
-    for(let i=0; i<collection.length; i++) {
-        let sourceKeyLength = Object.keys(source).length;
+    return collection.filter(item => {
         let k = 0;
-        for(let key in source){
-            if(collection[i].hasOwnProperty(key)) {
-
-                if(collection[i][key] === source[key]) {
-                    k += 1;
-                    if(k === sourceKeyLength){
-                        arr.push(collection[i]);
-                    }
+        for(let key in source) {
+            if ((item.hasOwnProperty(key)) && (item[key] === source[key])){
+                k += 1;
+                if(k === Object.keys(source).length) {
+                    return true;
                 }
             }
         }
-    }
-    return arr;
+    });
 }
 
 console.log(whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 }));
+
+// 1st attempt
+// function whatIsInAName(collection, source) {
+//     const arr = [];
+//     for(let i=0; i<collection.length; i++) {
+//         let sourceKeyLength = Object.keys(source).length;
+//         let k = 0;
+//         for(let key in source){
+//             if(collection[i].hasOwnProperty(key)) {
+//
+//                 if(collection[i][key] === source[key]) {
+//                     k += 1;
+//                     if(k === sourceKeyLength){
+//                         arr.push(collection[i]);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     return arr;
+// }
