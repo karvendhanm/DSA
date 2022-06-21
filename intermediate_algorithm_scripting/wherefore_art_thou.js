9dab476ec828c5b90c6c88f8d19096d88c78388b
Karvendhan M <karvsmech@gmail.com>
2022-06-21 21:34:36 +0530

    wherefore art thou, 1st attempt

--- intermediate_algorithm_scripting/wherefore_art_thou.js
+++ intermediate_algorithm_scripting/wherefore_art_thou.js
@@ -1,5 +1,22 @@
+// 1st attempt
 function whatIsInAName(collection, source) {
+    const arr = [];
+    for(let i=0; i<collection.length; i++) {
+        let sourceKeyLength = Object.keys(source).length;
+        let k = 0;
+        for(let key in source){
+            if(collection[i].hasOwnProperty(key)) {
 
+                if(collection[i][key] === source[key]) {
+                    k += 1;
+                    if(k === sourceKeyLength){
+                        arr.push(collection[i]);
+                    }
+                }
+            }
+        }
+    }
+    return arr;
 }
 
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+console.log(whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 }));
