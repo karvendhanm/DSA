c214f7ee14e1ac2126995e140752ebf974a2a35d
Karvendhan M <karvsmech@gmail.com>
2022-06-26 19:53:24 +0530

    spinal tap case

---
+++ intermediate_algorithm_scripting/spinal_tap_case.js
@@ -0,0 +1,9 @@
+function spinalCase(str) {
+
+}
+
+console.log(spinalCase('This Is Spinal Tap'));
+console.log(spinalCase('thisIsSpinalTap'));
+console.log(spinalCase('The_Andy_Griffith_Show'));
+console.log(spinalCase('Teletubbies say Eh-oh'));
+console.log(spinalCase('AllThe-small Things'));
