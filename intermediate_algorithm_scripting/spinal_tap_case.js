5fac013c03a4f6cbb33567a408fb3c49c86d6d38
Karvendhan M <karvsmech@gmail.com>
2022-07-05 20:10:42 +0530

    spinal tap case second attempt

--- intermediate_algorithm_scripting/spinal_tap_case.js
+++ intermediate_algorithm_scripting/spinal_tap_case.js
@@ -1,13 +1,11 @@
-// first attempt
+// second attempt
 function spinalCase(str) {
 //  Spinal tape case:
-
-    const regex_pattern0 = /[^a-zA-Z]/g;
-    const regex_pattern1 = /(?<!^)([A-Z])/g;
-    str = str.replace(regex_pattern0, '*');
-    str = str.replace(regex_pattern1, '*\$1');
+    const regex_pattern0 = /(?<!^)([A-Z])/g;
+    const regex_pattern1 = /[^a-zA-Z]+/g;
+    str = str.replace(regex_pattern0, '*\$1');
+    str = str.replace(regex_pattern1, '-');
     str = str.toLowerCase();
-    str = str.replace(/\*+/g, "-");
     return str;
 
 }
@@ -17,3 +15,17 @@ console.log(spinalCase('thisIsSpinalTap'));
 console.log(spinalCase('The_Andy_Griffith_Show'));
 console.log(spinalCase('Teletubbies say Eh-oh'));
 console.log(spinalCase('AllThe-small Things'));
+
+// first attempt
+// function spinalCase(str) {
+// //  Spinal tape case:
+//
+//     const regex_pattern0 = /[^a-zA-Z]/g;
+//     const regex_pattern1 = /(?<!^)([A-Z])/g;
+//     str = str.replace(regex_pattern0, '*');
+//     str = str.replace(regex_pattern1, '*\$1');
+//     str = str.toLowerCase();
+//     str = str.replace(/\*+/g, "-");
+//     return str;
+//
+// }
