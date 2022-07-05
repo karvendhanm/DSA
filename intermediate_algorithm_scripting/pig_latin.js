1d5aecf1eb5dfd2d70c60ec07fab83ba7b9a7909
Karvendhan M <karvsmech@gmail.com>
2022-07-05 21:05:15 +0530

    pig latin second attempt

--- intermediate_algorithm_scripting/pig_latin.js
+++ intermediate_algorithm_scripting/pig_latin.js
@@ -1,13 +1,20 @@
-// first attempt
+// second attempt
 function translatePigLatin(str) {
-    const regexPattern = /^[aeiou]/;
-    if(regexPattern.test(str)) {
-        str = str + 'way';
-    } else {
-        str = str.replace(/([^aeiou]+)(.*)/, '$2$1'+'ay');
-    }
-
+    str = str.replace(/^([aeiou])(.*)/, '$1$2' + 'way');
+    str = str.replace(/^([^aeiou]+)(.*)/, '$2$1'+'ay');
     return str;
 }
 
-console.log(translatePigLatin("consonant"));
+console.log(translatePigLatin("eight"));
+
+// first attempt
+// function translatePigLatin(str) {
+//     const regexPattern = /^[aeiou]/;
+//     if(regexPattern.test(str)) {
+//         str = str + 'way';
+//     } else {
+//         str = str.replace(/([^aeiou]+)(.*)/, '$2$1'+'ay');
+//     }
+//
+//     return str;
+// }
