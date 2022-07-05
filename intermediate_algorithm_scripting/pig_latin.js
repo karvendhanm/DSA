35930a9aeff5c73534e1133fe70694cfb2e07a0f
Karvendhan M <karvsmech@gmail.com>
2022-07-05 21:09:28 +0530

    pig latin third attempt

--- intermediate_algorithm_scripting/pig_latin.js
+++ intermediate_algorithm_scripting/pig_latin.js
@@ -1,11 +1,18 @@
-// second attempt
+// third attempt
 function translatePigLatin(str) {
-    str = str.replace(/^([aeiou])(.*)/, '$1$2' + 'way');
-    str = str.replace(/^([^aeiou]+)(.*)/, '$2$1'+'ay');
+    str = str.replace(/^([aeiou])(.*)/, '$&way');
+    str = str.replace(/^([^aeiou]+)(.*)/, '$2$1ay');
     return str;
 }
 
 console.log(translatePigLatin("eight"));
+
+// // second attempt
+// function translatePigLatin(str) {
+//     str = str.replace(/^([aeiou])(.*)/, '$1$2' + 'way');
+//     str = str.replace(/^([^aeiou]+)(.*)/, '$2$1'+'ay');
+//     return str;
+// }
 
 // first attempt
 // function translatePigLatin(str) {
