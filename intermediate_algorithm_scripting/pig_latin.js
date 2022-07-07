1f3770d74f7c5c390c2ef8a06a8b21a08bc757f4
Karvendhan M <karvsmech@gmail.com>
2022-07-07 08:18:00 +0530

    pig latin sixth attempt

--- intermediate_algorithm_scripting/pig_latin.js
+++ intermediate_algorithm_scripting/pig_latin.js
@@ -1,11 +1,17 @@
-// fifth attempt using recursion
+// sixth attempt using recursion
 function translatePigLatin(str, char_pos = 0) {
-     return (['a', 'e', 'i', 'o', 'u'].includes(str[0]) && (char_pos === 0)) ? str + 'way'
-         : ((['a', 'e', 'i', 'o', 'u'].includes(str[0])) || (char_pos === str.length)) ? str + 'ay'
-             : translatePigLatin(str.slice(1) + str[0], char_pos + 1)
+     return ['a', 'e', 'i', 'o', 'u'].includes(str[0]) ? str + (char_pos === 0 ? "way" : "ay")
+         : char_pos === str.length ? str + "ay" : translatePigLatin(str.slice(1) + str[0], char_pos + 1);
 }
 
 console.log(translatePigLatin("rhythm"));
+
+// // fifth attempt using recursion
+// function translatePigLatin(str, char_pos = 0) {
+//      return (['a', 'e', 'i', 'o', 'u'].includes(str[0]) && (char_pos === 0)) ? str + 'way'
+//          : ((['a', 'e', 'i', 'o', 'u'].includes(str[0])) || (char_pos === str.length)) ? str + 'ay'
+//              : translatePigLatin(str.slice(1) + str[0], char_pos + 1)
+// }
 
 // // fourth attempt
 // function translatePigLatin(str) {
