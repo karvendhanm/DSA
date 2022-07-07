e1334b7c02bab6861cb44cda245019820d289eea
Karvendhan M <karvsmech@gmail.com>
2022-07-07 09:22:56 +0530

    search and replace second attempt less readable

--- intermediate_algorithm_scripting/search_and_replace.js
+++ intermediate_algorithm_scripting/search_and_replace.js
@@ -1,18 +1,13 @@
-// first attempt
+// second attempt - Less code but not readable
 function myReplace(str, before, after) {
-    const regex_pattern = new RegExp(before, 'i');
-    if(regex_pattern.test(str)) {
-        let word = str.match(regex_pattern)[0];
-        if((word.charAt(0) === word.charAt(0).toUpperCase()) !== (after.charAt(0) === after.charAt(0).toUpperCase())){
-            if (after.charAt(0) === after.charAt(0).toUpperCase()) {
-                after = after.charAt(0).toLowerCase() + after.slice(1);
-            } else {
-                after = after.charAt(0).toUpperCase() + after.slice(1);
-            }
-        }
-    }
+
+        after = (before.charAt(0) === before.charAt(0).toUpperCase()) !== (after.charAt(0) === after.charAt(0).toUpperCase())
+            ? (after.charAt(0) === after.charAt(0).toUpperCase()
+                ? after.charAt(0).toLowerCase() : after.charAt(0).toUpperCase()) + after.slice(1) : after;
+
     return str.replaceAll(before, after);
 }
+
 
 console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
 console.log(myReplace("Let us go to the store", "store", "mall"));
@@ -21,3 +16,19 @@ console.log(myReplace("I think we should look up there", "up", "Down"));
 console.log(myReplace("This has a spellngi error", "spellngi", "spelling"));
 console.log(myReplace("His name is Tom", "Tom", "john"));
 console.log(myReplace("Let us get back to more Coding", "Coding", "algorithms"));
+
+// first attempt
+// function myReplace(str, before, after) {
+//     const regex_pattern = new RegExp(before, 'i');
+//     if(regex_pattern.test(str)) {
+//         let word = str.match(regex_pattern)[0];
+//         if((word.charAt(0) === word.charAt(0).toUpperCase()) !== (after.charAt(0) === after.charAt(0).toUpperCase())){
+//             if (after.charAt(0) === after.charAt(0).toUpperCase()) {
+//                 after = after.charAt(0).toLowerCase() + after.slice(1);
+//             } else {
+//                 after = after.charAt(0).toUpperCase() + after.slice(1);
+//             }
+//         }
+//     }
+//     return str.replaceAll(before, after);
+// }
