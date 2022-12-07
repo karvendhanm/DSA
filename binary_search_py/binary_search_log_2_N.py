f1bb58481561a2bf8492f6b021d586c432585644
karvendhanm <karvsmech@gmail.com>
2022-12-07 09:54:39 +0530

    binary search recursion attempt

--- binary_search_py/binary_search_log_2_N.py
+++ binary_search_py/binary_search_log_2_N.py
@@ -55,29 +55,6 @@ def binary_search_grokking(array, target):
     return None
 
 
-
-def binary_search_recursion(_lst, elem):
-    '''
-
-
-    :param _lst: a sorted list of numerals
-    :param elem: the element whose index needs to be found in the list: _lst
-    :return: the index of the elem in the _lst
-    :start_at:
-    '''
-
-    _len = len(_lst)
-    _idx = int(_len/2) if _len % 2 == 0 else int((_len-1)/2)
-    if _lst[_idx] != elem:
-        if elem > _lst[_idx]:
-            temp_idx = binary_search_recursion(_lst[_idx + 1:], elem)
-            _idx += (temp_idx + 1)
-        else:
-            _idx = binary_search_recursion(_lst[:_idx], elem)
-
-    return _idx
-
-
 _lst = [11, 34, 46, 57, 61, 63, 78, 81, 85, 86, 87, 99]
 elem_index = binary_search_grokking(_lst, 99)
 print(elem_index)
