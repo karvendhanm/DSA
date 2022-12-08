7482b7831eeee886ca4c3e0a907931f83fba256c
karvendhanm <karvsmech@gmail.com>
2022-12-08 09:21:36 +0530

    removing recursion logic for binary search

--- binary_search_py/binary_search.py
+++ binary_search_py/binary_search.py
@@ -25,31 +25,4 @@ def binary_search(array, target):
     return -1
 
 
-def binary_search_recursion(array, target, low = 0, high = 0):
-    '''
-
-    :param array:
-    :param target:
-    :return:
-    '''
-
-    if low == 0 and high == 0:
-        high = len(array) - 1
-
-    mid = math.floor((low + high) / 2)
-    guess = array[mid]
-
-    if guess == target:
-        return mid
-    elif guess > target:
-        tmp = binary_search_recursion(array, target, low=low, high=mid-1)
-    else:
-        tmp = binary_search_recursion(array, target, low=mid+1, high=high)
-
-    if tmp != -1:
-        return tmp
-
-    return -1
-
-
-print(binary_search_recursion([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33))
+print(binary_search([5, 23, 111], 3))
--- unittests_py/unit_tests.py
+++ unittests_py/unit_tests.py
@@ -12,4 +12,58 @@ class TestProgram(unittest.TestCase):
     def test_case_3(self):
         self.assertNotEqual(binary_search([1, 2, 3, 4, 6, 7, 8], 4), -1)
 
+    def test_case_4(self):
+        self.assertEqual(binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33), 3)
+
+    def test_case_5(self):
+        self.assertEqual(binary_search([1, 5, 23, 111], 111), 3)
+
+    def test_case_6(self):
+        self.assertEqual(binary_search([1, 5, 23, 111], 5), 1)
+
+    def test_case_7(self):
+        self.assertEqual(binary_search([1, 5, 23, 111], 35), -1)
+
+    def test_case_8(self):
+        self.assertEqual(binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 0), 0)
+
+    def test_case_9(self):
+        self.assertEqual(binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 1), 1)
+
+    def test_case_10(self):
+        self.assertEqual(binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 21), 2)
+
+    def test_case_11(self):
+        self.assertEqual(binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 45), 4)
+
+    def test_case_12(self):
+        self.assertEqual(binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 61), 6)
+
+    def test_case_13(self):
+        self.assertEqual(binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 71), 7)
+
+    def test_case_14(self):
+        self.assertEqual(binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 72), 8)
+
+    def test_case_15(self):
+        self.assertEqual(binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 73), 9)
+
+    def test_case_16(self):
+        self.assertEqual(binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 70), -1)
+
+    def test_case_17(self):
+        self.assertEqual(binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73, 355], 355), 10)
+
+    def test_case_18(self):
+        self.assertEqual(binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73, 355], 354), -1)
+
+    def test_case_19(self):
+        self.assertEqual(binary_search([1, 5, 23, 111], 120), -1)
+
+    def test_case_20(self):
+        self.assertEqual(binary_search([5, 23, 111], 3), -1)
+
+
+
+
 
