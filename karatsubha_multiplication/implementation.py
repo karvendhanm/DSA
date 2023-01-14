00ba0365ea836afe9178b72eb421c28a5196d7ca
karvendhanm <karvsmech@gmail.com>
2023-01-14 16:41:22 +0530

    karatsubha optimization and testcases

--- karatsubha_multiplication/implementation.py
+++ karatsubha_multiplication/implementation.py
@@ -10,9 +10,6 @@ def karatsubha_multiplication(left_num, right_num):
     if not isinstance(left_num, int) or  not isinstance(right_num, int):
         return "two integers are expected as arguments for this function"
 
-    if not left_num > 0 and right_num > 0:
-        return "two integers must be greater than zero"
-
     _lst = [left_num, right_num]
     _lst.sort()
     left_num, right_num = _lst[0], _lst[1]
@@ -21,19 +18,14 @@ def karatsubha_multiplication(left_num, right_num):
     right_num_length = len(str(right_num))
 
     # identify the number of zeroes trailing the numbers
+    left_num_stripped, right_num_stripped = 0, 0
     if left_num != 0:
         left_num_stripped = int(str(left_num).rstrip('0'))
-    else:
-        left_num_stripped = 0
-
     left_num_stripped_length = len(str(left_num_stripped))
     left_len_diff = left_num_length - left_num_stripped_length
 
     if right_num != 0:
         right_num_stripped = int(str(right_num).rstrip('0'))
-    else:
-        right_num_stripped = 0
-
     right_num_stripped_length = len(str(right_num_stripped))
     right_len_diff = right_num_length - right_num_stripped_length
 
@@ -45,31 +37,22 @@ def karatsubha_multiplication(left_num, right_num):
         val = int(str(_product).ljust(len_product + left_len_diff + right_len_diff, '0'))
         return val
 
-    if left_num_length == 1:
-        _right_pow = right_num_length // 2
-        right_quotient = right_num // (10 ** _right_pow)
-        right_remainder = right_num % (10 ** _right_pow)
+    _left_pow = left_num_length // 2
+    left_quotient = left_num // (10 ** _left_pow)
+    left_remainder = left_num % (10 ** _left_pow)
 
-        first_part = 10 ** _right_pow * karatsubha_multiplication(left_num, right_quotient)
-        last_part = karatsubha_multiplication(left_num, right_remainder)
+    _right_pow = right_num_length // 2
+    right_quotient = right_num // (10 ** _right_pow)
+    right_remainder = right_num % (10 ** _right_pow)
 
-        return first_part + last_part
+    first_part = (10 ** (_left_pow + _right_pow)) * karatsubha_multiplication(left_quotient, right_quotient)
+    second_part = (10 ** _left_pow) * karatsubha_multiplication(left_quotient, right_remainder)
+    third_part = (10 ** _right_pow) * karatsubha_multiplication(left_remainder, right_quotient)
+    last_part = karatsubha_multiplication(left_remainder, right_remainder)
 
-    else:
-        _left_pow = left_num_length // 2
-        left_quotient = left_num // (10 ** _left_pow)
-        left_remainder = left_num % (10 ** _left_pow)
+    return first_part + second_part + third_part + last_part
 
-        _right_pow = right_num_length // 2
-        right_quotient = right_num // (10 ** _right_pow)
-        right_remainder = right_num % (10 ** _right_pow)
 
-        first_part = (10 ** (_left_pow + _right_pow)) * karatsubha_multiplication(left_quotient, right_quotient)
-        second_part = (10 ** _left_pow) * karatsubha_multiplication(left_quotient, right_remainder)
-        third_part = (10 ** _right_pow) * karatsubha_multiplication(left_remainder, right_quotient)
-        last_part = karatsubha_multiplication(left_remainder, right_remainder)
-
-        return first_part + second_part + third_part + last_part
 
 
 
---
+++ unittests_py/unit_tests_karatsubha_multiplication.py
@@ -0,0 +1,25 @@
+import unittest
+
+from karatsubha_multiplication import karatsubha_multiplication as km
+
+class TestKaratSubha(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(km(5, 6), 30)
+
+    def test_case_2(self):
+        self.assertEqual(km(0, 456), 0)
+
+    def test_case_3(self):
+        self.assertEqual(km(1, 12), 12)
+
+    def test_case_4(self):
+        self.assertEqual(
+            km(3141592653589793238462643383279502884197169399375105820974944592,
+               2718281828459045235360287471352662497757247093699959574966967627)
+             , 8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184)
+
+    def test_case_5(self):
+        self.assertEqual(km(12, 1), 12)
+
+    def test_case_6(self):
+        self.assertEqual(km(1, 0), 0)
