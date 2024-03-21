212ee531b47bcf107f46dd0fec078b5e799f5a2f
karvendhanm <karvsmech@gmail.com>
2024-03-21 20:28:34 +0530

    two number sum with time complexity of O(nlog(n))

--- two_number_sum.py
+++ two_number_sum.py
@@ -1,8 +1,9 @@
+# time complexity O(n**2)
 # def twoNumberSum(array, targetSum):
 #     """
 #
 #     :param array:
-#     :param target_sum:
+#     :param targetSum:
 #     :return:
 #     """
 #     for idx, i in enumerate(array[:-1]):
@@ -12,21 +13,52 @@
 #     return []
 
 
+# time complexity O(n)
 # degrees of freedom approach
-def twoNumberSum(array, targetSum):
-    """
+# def twoNumberSum(array, targetSum):
+#     """
+#
+#     :param array:
+#     :param targetSum:
+#     :return:
+#     """
+#     for x in array:
+#         y = targetSum - x
+#         if (x != y) & (y in array):
+#             return [x, y]
+#     return []
 
-    :param array:
-    :param target_sum:
-    :return:
-    """
-    for idx, x in enumerate(array):
-        y = targetSum - x
-        if (x != y) & (y in array):
-            return [x, y]
+
+# algoexpert solution2
+# time complexity O(n)
+# def twoNumberSum(array, targetSum):
+#     nums = {}
+#     for num in array:
+#         potentialMatch = targetSum - num
+#         if potentialMatch in nums:
+#             return [potentialMatch, num]
+#         else:
+#             nums[num] = True
+#     return []
+
+# time complexity O(nlogn)
+def twoNumberSum(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array)-1
+    while left < right:
+        if array[left] + array[right] == targetSum:
+            return [array[left], array[right]]
+        elif array[left] + array[right] > targetSum:
+            right -= 1
+        else:
+            left +=1
     return []
 
 
+
+
 array = [3, 5, -4, 8, 11, 1, -1, 6]
-targetSum = 244
-print(two_number_sum(array, targetSum))
+targetSum = 10
+print(twoNumberSum(array, targetSum))
+
