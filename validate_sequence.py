6683fbe6f0cd0acf6c010419ea6a174526dbd053
Karvendhan M <karvsmech@gmail.com>
2024-04-05 19:29:29 +0530

    Create validate_sequence.py
    validating subsequence from an array

---
+++ validate_sequence.py
@@ -0,0 +1,27 @@
+def isValidSubsequence(array, sequence):
+    # Write your code here.
+    seq_len = len(sequence)
+    arr_len = len(array)
+    if seq_len > arr_len:
+        return False
+
+    seq_idx, arr_idx = 0, 0
+    for seq in sequence[seq_idx:]:
+        for arr in array[arr_idx:]:
+            arr_idx += 1
+            if seq == arr:
+                seq_idx += 1
+                break
+            if arr_idx > arr_len - 1:
+                return False
+
+    if seq_idx == seq_len:
+        return True
+    return False
+
+
+array = [5, 1, 22, 25, 6, -1, 8, 10]
+sequence = [1, 6, -1, 10]
+
+
+print(isValidSubsequence(array, sequence))
