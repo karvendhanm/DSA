1b52cb174d7ed8e42338d35ab8a7d149cdad8b2f
Karvendhan M <karvsmech@gmail.com>
2024-04-06 18:13:40 +0530

    sorted squared array second iteration
    sorted squared array second iteration without using python inbuilt functions such as remove and insert.

--- sorted_squared_array.py
+++ sorted_squared_array.py
@@ -1,17 +1,35 @@
+# def sortedSquaredArray(array):
+#     # Write your code here.
+#     _lst = []
+#     while(len(array)):
+#         _max = max(array)
+#         _min = min(array)
+
+#         if abs(_max)**2 > abs(_min)**2:
+#             _lst.insert(0, abs(_max)**2)
+#             array.remove(_max)
+#         else:
+#             _lst.insert(0, abs(_min)**2)
+#             array.remove(_min)
+
+#     return _lst
+
+
 def sortedSquaredArray(array):
     # Write your code here.
-    _lst = []
-    while(len(array)):
-        _max = max(array)
-        _min = min(array)
+    arr_len = len(array)
+    _lst = [0] * arr_len
+    start = 0
+    end, _idx = arr_len - 1, arr_len - 1
 
-        if abs(_max)**2 > abs(_min)**2:
-            _lst.insert(0, abs(_max)**2)
-            array.remove(_max)
+    while start <= end:
+        if abs(array[start])**2 >= abs(array[end])**2:
+            _lst[_idx] = abs(array[start]) ** 2
+            start += 1
         else:
-            _lst.insert(0, abs(_min)**2)
-            array.remove(_min)
-
+            _lst[_idx] = abs(array[end]) ** 2
+            end -= 1
+        _idx -= 1
     return _lst
 
 
