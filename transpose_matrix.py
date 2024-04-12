220ceb78392399ec1251d70ac28d760a744b6455
Karvendhan M <karvsmech@gmail.com>
2024-04-12 09:48:45 +0530

    transpose matrix second iteration

--- transpose_matrix.py
+++ transpose_matrix.py
@@ -1,18 +1,31 @@
+# def transposeMatrix(matrix):
+#     # Write your code here.
+
+#     num_cols = len(matrix)
+#     num_rows = len(matrix[0])
+
+#     _lst = []
+#     for i in range(num_rows):
+#         _lst.append([0] * num_cols)
+
+#     for _idx, _list in enumerate(matrix):
+#         for list_idx, element in enumerate(_list):
+#             _lst[list_idx][_idx] = element
+
+#     return _lst
+
+
 def transposeMatrix(matrix):
     # Write your code here.
+    transposed_matrix = []
 
-    num_cols = len(matrix)
-    num_rows = len(matrix[0])
+    for col_idx in range(len(matrix[0])):
+        _lst = []
+        for row_idx in range(len(matrix)):
+            _lst.append(matrix[row_idx][col_idx])
+        transposed_matrix.append(_lst)
 
-    _lst = []
-    for i in range(num_rows):
-        _lst.append([0] * num_cols)
-
-    for _idx, _list in enumerate(matrix):
-        for list_idx, element in enumerate(_list):
-            _lst[list_idx][_idx] = element
-
-    return _lst
+    return transposed_matrix
 
 
 matrix = [
