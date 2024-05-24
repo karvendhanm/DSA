17256145947f8eaf16c5073fcc1ae0418eeb5b11
karvendhanm <karvsmech@gmail.com>
2024-05-24 20:00:41 +0530

    class photos first iteration

---
+++ class_photos.py
@@ -0,0 +1,22 @@
+def classPhotos(redShirtHeights, blueShirtHeights):
+    # Write your code here.
+    redShirtHeights.sort()
+    blueShirtHeights.sort()
+
+    if blueShirtHeights[0] == redShirtHeights[0]:
+        return False
+
+    if blueShirtHeights[0] > redShirtHeights[0]:
+        redShirtHeights, blueShirtHeights = blueShirtHeights, redShirtHeights
+
+    for _idx, red_height in enumerate(redShirtHeights):
+        if red_height <= blueShirtHeights[_idx]:
+            return False
+
+    return True
+
+
+redShirtHeights = [5, 8, 1, 3, 4]
+blueShirtHeights = [6, 9, 2, 4, 5]
+
+classPhotos(redShirtHeights, blueShirtHeights)
