501fcaeb42e0a58865ef33059cff8e8e35d8b0ff
karvendhanm <karvsmech@gmail.com>
2024-05-25 19:14:22 +0530

    tandem bicycle first iteration

---
+++ tandem_bicycle.py
@@ -0,0 +1,18 @@
+def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
+    # Write your code here.
+
+    redShirtSpeeds.sort(reverse=False)
+    blueShirtSpeeds.sort(reverse=fastest)
+
+    total_speed = 0
+    for _idx, speed in enumerate(redShirtSpeeds):
+        total_speed += max(speed, blueShirtSpeeds[_idx])
+
+    return total_speed
+
+
+redShirtSpeeds = [3, 6, 7, 2, 1]
+blueShirtSpeeds = [5, 5, 3, 9, 2]
+fastest = False
+
+print(tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest))
