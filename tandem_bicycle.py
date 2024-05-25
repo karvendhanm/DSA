278e43e315c0c61c06fd0fd1e8ce3c9a64fdb86e
karvendhanm <karvsmech@gmail.com>
2024-05-25 19:18:30 +0530

    tandem bicycle seconf iteration

--- tandem_bicycle.py
+++ tandem_bicycle.py
@@ -1,14 +1,24 @@
+# def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
+#     # Write your code here.
+#
+#     redShirtSpeeds.sort(reverse=False)
+#     blueShirtSpeeds.sort(reverse=fastest)
+#
+#     total_speed = 0
+#     for _idx, speed in enumerate(redShirtSpeeds):
+#         total_speed += max(speed, blueShirtSpeeds[_idx])
+#
+#     return total_speed
+
+
+# second iteration
 def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
     # Write your code here.
 
     redShirtSpeeds.sort(reverse=False)
     blueShirtSpeeds.sort(reverse=fastest)
 
-    total_speed = 0
-    for _idx, speed in enumerate(redShirtSpeeds):
-        total_speed += max(speed, blueShirtSpeeds[_idx])
-
-    return total_speed
+    return sum([max(speed, blueShirtSpeeds[_idx]) for _idx, speed in enumerate(redShirtSpeeds)])
 
 
 redShirtSpeeds = [3, 6, 7, 2, 1]
