df31f2f0c460e23431adac449f072ec370ca98f4
Karvendhan M <karvsmech@gmail.com>
2024-06-11 17:44:09 +0530

    Update binary_search.py second iteration and space time complexity

--- binary_search.py
+++ binary_search.py
@@ -1,21 +1,31 @@
-def binarySearch(array, target, overall_idx=0):
-    # Write your code here.
-    current_idx = len(array)//2
+# O(log n) time | O(log n) space
+#1st iteration
+# def binarySearch(array, target, overall_idx=0):
+#     # Write your code here.
+#     current_idx = len(array)//2
+#
+#     if (array[current_idx] > target) & (len(array) > 1):
+#         return binarySearch(array[:current_idx], target, overall_idx)
+#     elif (array[current_idx] < target) & (len(array) > 1):
+#         return binarySearch(array[current_idx:], target, overall_idx+current_idx)
+#     elif array[current_idx] == target:
+#         return overall_idx + current_idx
+#     return -1
 
-    if (array[current_idx] > target) & (len(array) > 1):
-        return binarySearch(array[:current_idx], target, overall_idx)
-    elif (array[current_idx] < target) & (len(array) > 1):
-        return binarySearch(array[current_idx:], target, overall_idx+current_idx)
-    elif array[current_idx] == target:
-        return overall_idx + current_idx
+
+# iterative approach - 2nd iteration
+# O(log n) time | O(1) space
+def binarySearch(array, target):
+    left_idx = 0
+    right_idx = len(array)-1
+
+    while left_idx <= right_idx:
+        mid_idx = (left_idx + right_idx) // 2
+        if array[mid_idx] == target:
+            return mid_idx
+        elif array[mid_idx] > target:
+            right_idx = mid_idx - 1
+        elif array[mid_idx] < target:
+            left_idx = mid_idx + 1
+
     return -1
-
-
-array = [1, 5, 23, 111]
-temp = binarySearch(array, 33)
-print(temp)
-
-array = [0, 1, 21, 33, 45, 46, 61, 71, 72, 73, 76]
-temp = binarySearch(array, 76)
-print(temp)
-print('this is just for debugging purposes')
