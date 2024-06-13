f4e00538e0839109a3b53167edabb009e7a78d2e
Karvendhan M <karvsmech@gmail.com>
2024-06-13 20:03:54 +0530

    Update bubble_sort.py 2nd iteration

--- bubble_sort.py
+++ bubble_sort.py
@@ -8,3 +8,18 @@ def bubbleSort(array):
             if array[j + 1] < array[j]:
                 array[j], array[j + 1] = array[j + 1], array[j]
     return array
+
+
+# second iteration
+# O(n ** 2) time | O(1) space
+def bubbleSort(array):
+    _sorted = True
+    counter = 0
+    while _sorted:
+        _sorted = False
+        for i in range(len(array) - 1 - counter):
+            if array[i] > array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+                _sorted = True
+        counter += 1
+    return array
