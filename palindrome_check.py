f00de71feabbc2759275539d7db6033aaf43a1ef
Karvendhan M <karvsmech@gmail.com>
2024-06-20 19:39:19 +0530

    Update palindrome_check.py

--- palindrome_check.py
+++ palindrome_check.py
@@ -1,27 +1,34 @@
 # first iteration
 # O(n) time |  O(1) space
-def isPalindrome(string):
-    # Write your code here.
-    start_idx = 0
-    end_idx = len(string) - 1
-
-    while start_idx <= end_idx:
-        if string[start_idx] != string[end_idx]:
-            return False
-        start_idx += 1
-        end_idx -= 1
-    return True
+# def isPalindrome(string):
+#     # Write your code here.
+#     start_idx = 0
+#     end_idx = len(string) - 1
+#
+#     while start_idx <= end_idx:
+#         if string[start_idx] != string[end_idx]:
+#             return False
+#         start_idx += 1
+#         end_idx -= 1
+#     return True
 
 
 # recursive way of solving the palindrome check.
 # O(n) time |  O(n) space
-# O(n) time |  O(1) space if we consider tail recursion or tail calls
-def isPalindrome(string):
+# need to find out if slicing a string creates a new string.
+# def isPalindrome(string):
+#
+#     if len(string) <= 1:
+#         return True
+#
+#     if string[0] != string[-1]:
+#         return False
+#
+#     return isPalindrome(string[1:-1])
 
-    if len(string) <= 1:
-        return True
 
-    if string[0] != string[-1]:
-        return False
-
-    return isPalindrome(string[1:-1])
+# another way of writing the recursion without slicing a string
+# O(n) time |  O(1) space
+def isPalindrome(string, i=0):
+    j = len(string) - 1 - i
+    return True if i >= j else string[i] == string[j] and isPalindrome(string, i + 1)
