5fd555f00a6afd9499da49281633f2db4396b401
karvendhanm <karvsmech@gmail.com>
2024-06-21 19:36:58 +0530

    caesar cipher second iteration

--- caesar_cipher_encryptor.py
+++ caesar_cipher_encryptor.py
@@ -1,24 +1,34 @@
 # first iteration
 # O(n) time | O(1) space
+# def caesarCipherEncryptor(string, key):
+#     # Write your code here.
+#     alphabets = list("abcdefghijklmnopqrstuvwxyz")
+#
+#     _lst = []
+#     for elem in string:
+#         for _idx, char in enumerate(alphabets):
+#             if char == elem:
+#                 req_idx = _idx + key
+#                 req_idx = req_idx if req_idx <= 25 else (req_idx % 26)
+#                 _lst.append(alphabets[req_idx])
+#                 break
+#
+#     return "".join(_lst)
+
+
+# second iteration
+# O(n) time | O(1) space
 def caesarCipherEncryptor(string, key):
     # Write your code here.
-    alphabets = list("abcdefghijklmnopqrstuvwxyz")
-
     _lst = []
     for elem in string:
-        for _idx, char in enumerate(alphabets):
-            if char == elem:
-                req_idx = _idx + key
-                req_idx = req_idx if req_idx <= 25 else (req_idx % 26)
-                _lst.append(alphabets[req_idx])
-                break
-
+        num = ord(elem) + key
+        num = (((num - 97) % 26) + 97) if num > 122 else num
+        _lst.append(chr(num))
     return "".join(_lst)
+
 
 string = 'xyz'
 key = 2
 
-
 print(caesarCipherEncryptor(string, key))
-
-
