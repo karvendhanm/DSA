e9205e3b194301cb3d69f3b03b2296d88685d29f
Karvendhan M <karvsmech@gmail.com>
2024-06-25 18:06:10 +0530

    Update run_length_encoding.py fourth iteration

--- run_length_encoding.py
+++ run_length_encoding.py
@@ -32,3 +32,43 @@ def runLengthEncoding(string):
             _str += str(_newValIdx - _idx) + string[_idx]
             _idx = _newValIdx
     return _str
+
+
+# O(n) time | O(n) space
+# third iteration
+def runLengthEncoding(string):
+    _idx, _newValIdx = 0, 0
+    string = list(string)
+    chars = []
+    while _newValIdx <= len(string)-1:
+        _newValIdx += 1
+        if _newValIdx == len(string) or string[_newValIdx] != string[_idx] or (string[_newValIdx] == string[_idx] and
+                                                                               (_newValIdx - _idx) == 9):
+            chars.append(str(_newValIdx - _idx))
+            chars.append(string[_idx])
+
+            _idx = _newValIdx
+    return "".join(chars)
+
+
+# O(n) time | O(n) space
+# fourth iteration
+def runLengthEncoding(string):
+    length = 1   # we are guaranteed a non-empty string.
+    encodedStringCharacters = []
+
+    for i in range(1, len(string)):
+        currentChar = string[i]
+        previousChar = string[i-1]
+
+        if currentChar != previousChar or length == 9:
+            encodedStringCharacters.append(str(length))
+            encodedStringCharacters.append(previousChar)
+            length = 0
+
+        length += 1
+
+    encodedStringCharacters.append(str(length))
+    encodedStringCharacters.append(string[-1])
+
+    return "".join(encodedStringCharacters)
