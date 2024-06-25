fd0d55d16bd9de200ab9b411591f017bd87143c0
Karvendhan M <karvsmech@gmail.com>
2024-06-25 17:11:27 +0530

    Update run_length_encoding.py second iteration

--- run_length_encoding.py
+++ run_length_encoding.py
@@ -18,3 +18,17 @@ def runLengthEncoding(string):
     for cnt, alphabet in zip(countLst, letterLst):
         final_str += str(cnt) + alphabet
     return final_str
+
+
+# second iteration
+def runLengthEncoding(string):
+    _idx, _newValIdx = 0, 0
+    string = list(string)
+    _str = ""
+    while _newValIdx <= len(string)-1:
+        _newValIdx += 1
+        if _newValIdx == len(string) or string[_newValIdx] != string[_idx] or (string[_newValIdx] == string[_idx] and
+                                                                               (_newValIdx - _idx) == 9):
+            _str += str(_newValIdx - _idx) + string[_idx]
+            _idx = _newValIdx
+    return _str
