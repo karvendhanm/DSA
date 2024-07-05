6d8c881a08031faf691025a7e59f8adfe6b04b45
Karvendhan M <karvsmech@gmail.com>
2024-07-05 17:55:12 +0530

    Update common_characters.py three iterations

--- common_characters.py
+++ common_characters.py
@@ -26,3 +26,23 @@ def commonCharacters(strings):
                 final_lst.remove(letter)
         letters = [letter for letter in final_lst]
     return final_lst
+
+
+# O(n * m) time - where n is the numbers of strings in the input list and m is the max. number of characters in a string
+# O(s) space - where s is the number of unique characters in all the strings put together.
+# third iteration
+# def commonCharacters(strings):
+#     # Write your code here.
+#     _character_count = {}
+#     for string in strings:
+#         _lst = set(list(string))
+#         for char in _lst:
+#             if char not in _character_count:
+#                 _character_count[char] = 1
+#             else:
+#                 _character_count[char] += 1
+#     _lst = []
+#     for key, val in _character_count.items():
+#         if val == len(strings):
+#             _lst.append(key)
+#     return _lst
