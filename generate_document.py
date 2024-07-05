f9926f467e699c6c5f34a6c6b350bfc9a6964ef7
karvendhanm <karvsmech@gmail.com>
2024-07-05 18:45:58 +0530

    update generate_document second iteration

--- generate_document.py
+++ generate_document.py
@@ -2,19 +2,43 @@
 # O(mn) time | O(m) + O(n) space
 # where m - number of characters in "characters" string.
 # where n - number of characters in "document" string.
+# def generateDocument(characters, document):
+#     # Write your code here.
+#     if document == "":
+#         return True
+#
+#     characters = list(characters)
+#     document = list(document)
+#
+#     for _char in document:
+#         if _char not in characters:
+#             return False
+#         characters.remove(_char)
+#     return True
+
+
+# O(m) + O(n) time | O(c) space
+# where m - number of characters in "characters" string.
+# where n - number of characters in "document" string.
+# where c - number of unique characters in "characters" string.
+# second iteration
 def generateDocument(characters, document):
     # Write your code here.
     if document == "":
         return True
 
-    characters = list(characters)
-    document = list(document)
+    char_dict = {}
+    for _char in characters:
+        if _char not in char_dict:
+            char_dict[_char] = 0
+        char_dict[_char] += 1
 
     for _char in document:
-        if _char not in characters:
+        if _char not in char_dict or char_dict[_char] <= 0:
             return False
-        characters.remove(_char)
+        char_dict[_char] -= 1
     return True
+
 
 
 characters = "Bste!hetsi ogEAxpelrt x "
