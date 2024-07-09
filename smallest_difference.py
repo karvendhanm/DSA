8e24717c9ff2a884bcba258fc395dc583e238926
karvendhanm <karvsmech@gmail.com>
2024-07-09 05:51:31 +0530

    create smallest_difference.py

---
+++ smallest_difference.py
@@ -0,0 +1,8 @@
+def smallestDifference(arrayOne, arrayTwo):
+    # Write your code here.
+    difference = float('inf')
+    pass
+
+arrayOne = [-1, 5, 10, 20, 28, 3]
+arrayTwo = [26, 134, 135, 15, 17]
+smallestDifference(arrayOne, arrayTwo)
