5901bba607e815b7abc384d14e7b1a5f8429a943
karvendhanm <karvsmech@gmail.com>
2024-07-10 05:29:15 +0530

    update smallest_difference.py second iteration

--- smallest_difference.py
+++ smallest_difference.py
@@ -1,17 +1,41 @@
 # first iteration
 # O(n^2) time | O(1) space
+# def smallestDifference(arrayOne, arrayTwo):
+#     # Write your code here.
+#     difference = float('inf')
+#     _lst = []
+#     for num1 in arrayOne:
+#         for num2 in arrayTwo:
+#             if abs(num1 - num2) < difference:
+#                 difference = abs(num1 - num2)
+#                 _lst = [num1, num2]
+#     return _lst
+
+
+# second iteration
+# O(nlog(n)) time | O(1) space
 def smallestDifference(arrayOne, arrayTwo):
-    # Write your code here.
-    difference = float('inf')
+    arrayOne.sort()                                                                 # O(nlog(n)) time
+    arrayTwo.sort()                                                                 # O(nlog(n)) time
+
     _lst = []
-    for num1 in arrayOne:
-        for num2 in arrayTwo:
-            if abs(num1 - num2) < difference:
-                difference = abs(num1 - num2)
-                _lst = [num1, num2]
+    difference = float('inf')
+    _idx1, _idx2 = 0, 0
+    while _idx1 <= len(arrayOne) - 1 and _idx2 <= len(arrayTwo) - 1:                # O(n) time
+        if abs(arrayOne[_idx1] - arrayTwo[_idx2]) < difference:
+            difference = abs(arrayOne[_idx1] - arrayTwo[_idx2])
+            _lst = [arrayOne[_idx1], arrayTwo[_idx2]]
+
+        if arrayOne[_idx1] > arrayTwo[_idx2]:
+            _idx2 += 1
+        elif arrayOne[_idx1] < arrayTwo[_idx2]:
+            _idx1 += 1
+        else:
+            return _lst
     return _lst
 
 
-arrayOne = [-1, 5, 10, 20, 28, 3]
-arrayTwo = [26, 134, 135, 15, 17]
+
+arrayOne = [240, 124, 86, 111, 2, 84, 954, 27, 89]
+arrayTwo = [1, 3, 954, 19, 8]
 print(smallestDifference(arrayOne, arrayTwo))
