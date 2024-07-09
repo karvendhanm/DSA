9ab85428bce5fcd790da75287a33afc457786e6b
karvendhanm <karvsmech@gmail.com>
2024-07-10 04:53:55 +0530

    create smallest difference first iteration

--- smallest_difference.py
+++ smallest_difference.py
@@ -1,8 +1,17 @@
+# first iteration
+# O(n^2) time | O(1) space
 def smallestDifference(arrayOne, arrayTwo):
     # Write your code here.
     difference = float('inf')
-    pass
+    _lst = []
+    for num1 in arrayOne:
+        for num2 in arrayTwo:
+            if abs(num1 - num2) < difference:
+                difference = abs(num1 - num2)
+                _lst = [num1, num2]
+    return _lst
+
 
 arrayOne = [-1, 5, 10, 20, 28, 3]
 arrayTwo = [26, 134, 135, 15, 17]
-smallestDifference(arrayOne, arrayTwo)
+print(smallestDifference(arrayOne, arrayTwo))
