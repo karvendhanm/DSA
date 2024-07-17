277fb3719a2a9e40f212d8857b30dc4114529bc2
Karvendhan M <karvsmech@gmail.com>
2024-07-17 16:41:14 +0530

    Update monotonic_array.py second iteration

--- monotonic_array.py
+++ monotonic_array.py
@@ -1,13 +1,28 @@
+# O(n) time | O(n) space.
+# def isMonotonic(array):
+#     # Write your code here.
+#     if len(array) <= 1:
+#         return True
+#
+#     _lst = []                                                                       # O(n) space
+#     for i in range(len(array) - 1):                                                 # O(n) time
+#         _lst.append(array[i+1] - array[i])
+#
+#     if all(elem>= 0 for elem in _lst) or all(elem <= 0 for elem in _lst):           # O(n) time
+#         return True
+#
+#     return False
+
+
+# second iteration
+# O(n) time | O(1) space.
 def isMonotonic(array):
-    # Write your code here.
-    if len(array) <= 1:
-        return True
 
-    _lst = []
-    for i in range(len(array) - 1):
-        _lst.append(array[i+1] - array[i])
-
-    if len([True for elem in _lst if elem >= 0]) == len(array) - 1 or len([True for elem in _lst if elem <= 0]) == len(array) - 1:
-        return True
-
-    return False
+    isNonDecreasing = True
+    isNonIncreasing = True
+    for i in range(1, len(array)):
+        if array[i] > array[i-1]:
+            isNonIncreasing = False
+        if array[i] < array[i-1]:
+            isNonDecreasing = False
+    return isNonDecreasing or isNonIncreasing
