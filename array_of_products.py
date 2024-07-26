de45ab569cfc9f6b670db7ea976264be605dd157
Karvendhan M <karvsmech@gmail.com>
2024-07-26 08:59:47 +0530

    Update array_of_products.py third iteration

--- array_of_products.py
+++ array_of_products.py
@@ -16,25 +16,50 @@
 
 # second iteration
 # O(n) time | O(n) space
+# def arrayOfProducts(array):
+#     # Write your code here.
+#     leftSideProductArray = [1 for _ in range(len(array))]
+#     rightSideProductArray = [1 for _ in range(len(array))]
+#
+#     # filling up leftSideProductArray
+#     product = 1
+#     for i in range(len(array)):
+#         leftSideProductArray[i] = product
+#         product *= array[i]
+#
+#     # filling up rightSideProductArray
+#     product = 1
+#     for i in reversed(range(len(array))):
+#         rightSideProductArray[i] = product
+#         product *= array[i]
+#
+#     result = []
+#     for i in range(len(array)):
+#         result.append(leftSideProductArray[i] * rightSideProductArray[i])
+#
+#     return result
+
+
+# third iteration
+# O(n) time | O(n) space
 def arrayOfProducts(array):
     # Write your code here.
-    leftSideProductArray = [1 for _ in range(len(array))]
-    rightSideProductArray = [1 for _ in range(len(array))]
+    result = [1 for _ in range(len(array))]
 
     # filling up leftSideProductArray
     product = 1
     for i in range(len(array)):
-        leftSideProductArray[i] = product
+        result[i] = product
         product *= array[i]
 
-    # filling up rightSideProductArray
     product = 1
     for i in reversed(range(len(array))):
-        rightSideProductArray[i] = product
+        result[i] *= product
         product *= array[i]
 
-    result = []
-    for i in range(len(array)):
-        result.append(leftSideProductArray[i] * rightSideProductArray[i])
-
     return result
+
+
+array = [5, 1, 4, 2]
+print(arrayOfProducts(array))
+
