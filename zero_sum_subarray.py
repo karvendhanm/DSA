930fa05e303e13f4abbe51e89b4cae1ec94e0579
karvendhanm <karvsmech@gmail.com>
2024-08-11 16:37:57 +0530

    update zero_sum_subarray.py second iteration

--- zero_sum_subarray.py
+++ zero_sum_subarray.py
@@ -1,15 +1,34 @@
 # first iteration
 # O(n^2) time | O(1) space
+# def zeroSumSubarray(nums):
+#     # Write your code here.
+#     for outer_idx, outer_elem in enumerate(nums):
+#         count = outer_elem
+#         if count == 0:
+#             return True
+#         for inner_idx in range(outer_idx+1, len(nums)):
+#             count += nums[inner_idx]
+#             if count == 0:
+#                 return True
+#     return False
+
+
+# second iteration
+# O(n) time | O(n) space
 def zeroSumSubarray(nums):
     # Write your code here.
-    for outer_idx, outer_elem in enumerate(nums):
-        count = outer_elem
-        if count == 0:
+    sumArray = []
+    sum = 0
+    for elem in nums:
+        sum += elem
+        if sum == 0:
             return True
-        for inner_idx in range(outer_idx+1, len(nums)):
-            count += nums[inner_idx]
-            if count == 0:
-                return True
+        sumArray.append(sum)
+
+    for _idx, elem in enumerate(sumArray):
+        if elem in sumArray[_idx+1:]:
+            return True
+
     return False
 
 
