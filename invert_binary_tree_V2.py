b91935546fc98da597f1b7527a6e7c7f6402bcaa
karvendhanm <karvsmech@gmail.com>
2024-09-13 05:49:23 +0530

    create invert binary tree iterative solution

---
+++ invert_binary_tree_V2.py
@@ -0,0 +1,27 @@
+# This is the class of the input binary tree.
+class BT:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+tree = BT(1)
+tree.left = BT(2)
+tree.left.left = BT(4)
+tree.left.right = BT(5)
+tree.left.left.left = BT(8)
+tree.left.left.rigth = BT(8)
+tree.right = BT(3)
+tree.right.left = BT(6)
+tree.right.right = BT(7)
+
+
+# second iteration iterative solution
+# O() time | O() space
+def invertBinaryTree(tree):
+    # Write your code here.
+    pass
+
+
+invertBinaryTree(tree)
