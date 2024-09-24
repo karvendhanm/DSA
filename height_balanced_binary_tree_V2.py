2ceb088ff6b626296342df0792c37b72e83354b2
Karvendhan M <karvsmech@gmail.com>
2024-09-24 16:58:08 +0530

    Create height_balanced_binary_tree_V2.py 2nd iteration optimal time and space

---
+++ height_balanced_binary_tree_V2.py
@@ -0,0 +1,50 @@
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+rootNode = BinaryTree(1)
+# left
+rootNode.left = BinaryTree(2)
+rootNode.left.left = BinaryTree(4)
+rootNode.left.right = BinaryTree(5)
+rootNode.left.right.left = BinaryTree(7)
+rootNode.left.right.right = BinaryTree(8)
+
+# right
+rootNode.right = BinaryTree(3)
+rootNode.right.right = BinaryTree(6)
+rootNode.right.right.left = BinaryTree(9)
+rootNode.right.right.left.left = BinaryTree(10)
+# rootNode.right.right.right = BinaryTree(10)
+
+class BalancedBinaryTree(object):
+    def __init__(self, flag):
+        self.flag = flag
+
+
+def heightBalancedBinaryTreeHelper(tree, obj):
+    if tree is None or not obj.flag:
+        return 0
+
+    _left = heightBalancedBinaryTreeHelper(tree.left, obj)
+    _right = heightBalancedBinaryTreeHelper(tree.right, obj)
+
+    if abs(_left - _right) > 1 and obj.flag:
+        obj.flag = False
+
+    return max(_left, _right) + 1
+
+
+# second iteration
+# O(n) time | O(h) space
+def heightBalancedBinaryTree(tree):
+    # Write your code here.
+    obj = BalancedBinaryTree(True)
+    heightBalancedBinaryTreeHelper(tree, obj)
+    return obj.flag
+
+
+print(heightBalancedBinaryTree(rootNode))
