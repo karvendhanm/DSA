5c60563d7d90235592b44f209c0fcda88585c8c1
karvendhanm <karvsmech@gmail.com>
2024-09-26 05:07:30 +0530

    merge_binary_trees.py first iteration

--- merge_binary_trees.py
+++ merge_binary_trees.py
@@ -27,9 +27,32 @@ tree2.right.left = BinaryTree(7)
 tree2.right.right = BinaryTree(6)
 
 
+# first iteration
+def mergeBinaryTreeshelper(tree1, tree2):
+    if tree1 is None and tree2 is None:
+        return None
+
+    if tree1.left is None and tree2.left is not None:
+        tree1.left = BinaryTree(0)
+    elif tree1.left is not None and tree2.left is None:
+        tree2.left = BinaryTree(0)
+
+    if tree1.right is None and tree2.right is not None:
+        tree1.right = BinaryTree(0)
+    elif tree1.right is not None and tree2.right is None:
+        tree2.right = BinaryTree(0)
+
+    tree1.left = mergeBinaryTreeshelper(tree1.left, tree2.left)
+    tree1.right = mergeBinaryTreeshelper(tree1.right, tree2.right)
+
+    tree1.value += tree2.value
+
+    return tree1
+
+
 def mergeBinaryTrees(tree1, tree2):
     # Write your code here.
-    return None
+    return mergeBinaryTreeshelper(tree1, tree2)
 
 
 mergeBinaryTrees(tree1, tree2)
