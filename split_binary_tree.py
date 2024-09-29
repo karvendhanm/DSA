ba8bc2c38f345d6abc85a6ea5c54156d0b60275f
karvendhanm <karvsmech@gmail.com>
2024-09-30 05:20:47 +0530

    split_binary_tree.py first iteration

---
+++ split_binary_tree.py
@@ -0,0 +1,59 @@
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+# tree 1
+# tree = BinaryTree(1)
+# # left subtree
+# tree.left = BinaryTree(3)
+# tree.left.right = BinaryTree(-5)
+# tree.left.left = BinaryTree(6)
+# tree.left.left.left = BinaryTree(2)
+#
+# # right subtree
+# tree.right = BinaryTree(-2)
+# tree.right.left = BinaryTree(5)
+# tree.right.right = BinaryTree(2)
+
+
+# tree 1
+tree = BinaryTree(1)
+# left subtree
+tree.left = BinaryTree(1)
+tree.left.left = BinaryTree(12)
+tree.left.left.right = BinaryTree(-21)
+
+# right subtree
+tree.right = BinaryTree(2)
+tree.right.right = BinaryTree(-5)
+
+
+# first iteration
+def splitBinaryTreeHelper(tree, _lst):
+    if tree is None:
+        return 0
+
+    _left = splitBinaryTreeHelper(tree.left, _lst)
+    _right = splitBinaryTreeHelper(tree.right, _lst)
+
+    tot_val = _left + _right + tree.value
+    _lst.append(tot_val)
+
+    return tot_val
+
+
+def splitBinaryTree(tree):
+    # Write your code here.
+    _lst = []
+    splitBinaryTreeHelper(tree, _lst)
+    max_elem = _lst[-1]
+    for elem in _lst:
+        if 2 * elem == max_elem:
+            return elem
+    return 0
+
+
+splitBinaryTree(tree)
