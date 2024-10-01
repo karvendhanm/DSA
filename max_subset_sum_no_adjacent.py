c1ea15f5a9684b9e6086a2aa8cdd3747c979e63c
karvendhanm <karvsmech@gmail.com>
2024-10-01 07:22:52 +0530

    max_subset_sum_no_adjacent.py two iterations

--- max_subset_sum_no_adjacent.py
+++ max_subset_sum_no_adjacent.py
@@ -1,7 +1,36 @@
+# first iteration
+# O(n) time | O(n) space
+# def maxSubsetSumNoAdjacent(array):
+#     # Write your code here.
+#     if not len(array):
+#         return 0
+#     if len(array) == 1:
+#         return array[0]
+#     maxSums = array[:]
+#     maxSums[1] = max(array[0], array[1])
+#
+#     for i in range(2, len(maxSums)):
+#         maxSums[i] = max(maxSums[i-1], maxSums[i-2]+array[i])
+#     return maxSums[-1]
+
+
+# second iteration
+# O(n) time | O(1) space
 def maxSubsetSumNoAdjacent(array):
     # Write your code here.
-    pass
+    if not len(array):
+        return 0
+    if len(array) == 1:
+        return array[0]
+    first = array[0]
+    second = max(array[0], array[1])
+
+    for i in range(2, len(array)):
+        current = max(second, first + array[i])
+        first = second
+        second = current
+    return second
 
 
-array = [75, 105, 120, 75, 90, 135]
-maxSubsetSumNoAdjacent(array)
+array = [4, 3, 5, 200, 5, 3]
+print(maxSubsetSumNoAdjacent(array))
