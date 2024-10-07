54b7dc750b63e2faf592b37cd82105bbf437ee80
karvendhanm <karvsmech@gmail.com>
2024-10-07 06:03:26 +0530

    number_of_ways_to_make_change.py dynamic programming

--- number_of_ways_to_make_chage.py
+++ number_of_ways_to_make_chage.py
@@ -1,10 +1,19 @@
-def numberOfWaysToMakeChange(n, denoms):
+# O(nd) time | O(n) space
+# where n is the amount and d is the number of denominations
+def numberOfWaysToMakeChange(n, denoms,):
     # Write your code here.
-    pass
+    ways = [0 for _ in range(n+1)]
+    ways[0] = 1
+
+    for denom in denoms:
+        for amount in range(1, n+1):
+            if denom <= amount:
+                ways[amount] = ways[amount] + ways[amount - denom]
+    return ways[n]
 
 
-n = 6
-denoms = [1, 5]
+n = 10
+denoms = [1, 5, 10, 25]
 
 
-numberOfWaysToMakeChange(n, denoms)
+print(numberOfWaysToMakeChange(n, denoms))
