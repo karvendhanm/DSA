888d7f01d5e8e1c780636e7f50af882b35181d19
karvendhanm <karvsmech@gmail.com>
2024-10-09 06:27:35 +0530

    min_number_of_coins_for_change.py incorrect solution

--- min_number_of_coins_for_change.py
+++ min_number_of_coins_for_change.py
@@ -1,10 +1,23 @@
+# incorrect approach
 def minNumberOfCoinsForChange(n, denoms):
     # Write your code here.
-    pass
+    ways = [-1 for _ in range(n + 1)]
+    ways[0] = 0
+    denoms.sort()
+
+    for denom in denoms:
+        for amount in range(1, n + 1):
+            if amount >= denom:
+                q = amount // denom
+                r = amount % denom
+
+                current = q + ways[r] if ways[r] != -1 else -1
+                ways[amount] = min(ways[amount], current) if (current != -1 and ways[amount] != -1) else max(ways[amount], current)
+
+    return ways[n]
 
 
-n = 7
-denoms = [1, 5, 10]
+n = 10
+denoms = [1, 3, 4]
 
-
-minNumberOfCoinsForChange(7, denoms)
+print(minNumberOfCoinsForChange(n, denoms))
