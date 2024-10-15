4215be783505fa6be420ae627d1c5d4b03924970
karvendhanm <karvsmech@gmail.com>
2024-10-15 09:05:38 +0530

    levenshtein_distance.py first iteration sub-optimal space

--- levenshtein_distance.py
+++ levenshtein_distance.py
@@ -1,5 +1,20 @@
+# O(nm) time | O(nm) space
+# where n - length of the first string
+# where m - length of the second string
 def levenshteinDistance(str1, str2):
     # Write your code here.
-    pass
+    # in dynamic programming, build a 2d array
+    edits = [[x for x in range(len(str1) + 1)] for _ in range(len(str2) + 1)]
+    for i in range(1, len(str2) + 1):
+        edits[i][0] = edits[i-1][0] + 1
+    for i in range(1, len(str2) + 1):
+        for j in range(1, len(str1) + 1):
+            if str2[i-1] == str1[j-1]:
+                edits[i][j] = edits[i-1][j-1]
+            else:
+                edits[i][j] = 1 + min(edits[i-1][j-1], edits[i][j-1], edits[i-1][j])
+    return edits[-1][-1]
+
+
 
 levenshteinDistance('abc', 'yabd')
