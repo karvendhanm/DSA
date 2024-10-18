54ee9e573285d7270442df5a74e4687524607283
Karvendhan M <karvsmech@gmail.com>
2024-10-18 15:18:11 +0530

    Update union_find.py solution 2 better time complexity

--- union_find.py
+++ union_find.py
@@ -30,6 +30,48 @@ class UnionFind:
         return
 
 
+# solution 2
+class UnionFind:
+    def __init__(self):
+        self.parents = {}
+        self.rank = {}
+
+    # O(1) time and O(1) space
+    def createSet(self, value):
+        self.parents[value] = value
+        self.rank[value] = 0
+
+    # O(log n) time and O(1) space
+    def find(self, value):
+        # we are normally finding the parent or
+        # the representative element in the set.
+        if value not in self.parents:
+            return None
+
+        while value != self.parents[value]:
+            value = self.parents[value]
+        return value
+
+    # O(log n) time and O(1) space
+    def union(self, valueOne, valueTwo):
+        if valueOne not in self.parents or valueTwo not in self.parents:
+            return
+
+        valueOneParent = self.find(valueOne)
+        valueTwoParent = self.find(valueTwo)
+        rankParentOne = self.rank[valueOneParent]
+        rankParentTwo = self.rank[valueTwoParent]
+
+        if rankParentOne > rankParentTwo:
+            self.parents[valueTwoParent] = valueOneParent
+            self.rank[valueOneParent] += 1
+        else:
+            self.parents[valueOneParent] = valueTwoParent
+            self.rank[valueTwoParent] += 1
+
+        return
+
+
 obj = UnionFind()
 obj.createSet(3)
 obj.createSet(6)
