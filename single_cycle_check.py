ea925a8af3c7ea6e8bb087edfadc836e7d4f6492
karvendhanm <karvsmech@gmail.com>
2024-10-19 18:58:36 +0530

    second iteration optimal space

--- single_cycle_check.py
+++ single_cycle_check.py
@@ -14,5 +14,20 @@ def hasSingleCycle(array):
     return all(num == 1 for num in flags)
 
 
+# second iteration
+# O(n) time | O(1) space
+def hasSingleCycle(array):
+    # Write your code here.
+    arr_length = len(array)
+    pos, jumps = 0, 0
+    while True:
+        pos += array[pos]
+        pos = pos % arr_length
+        jumps += 1
+        if pos == 0 or jumps > arr_length or array[pos] == 0:
+            break
+    return jumps == arr_length
+
+
 array = [2, 3, 1, -4, -4, 2]
 hasSingleCycle(array)
