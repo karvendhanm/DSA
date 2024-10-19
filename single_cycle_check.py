3eeb2ef588434abd131be08d383fc7cac69a60e2
karvendhanm <karvsmech@gmail.com>
2024-10-19 06:12:51 +0530

    first working iteration sub-optimal space

---
+++ single_cycle_check.py
@@ -0,0 +1,33 @@
+# first iteration
+# O(n) time | O(n) space
+def hasSingleCycle(array):
+    # Write your code here.
+    arr_length = len(array)
+    flags = [0 for x in range(arr_length)]
+    winder = arr_length
+
+    pos = 0
+    while winder > 0:
+        pos += array[pos]
+
+        if pos > arr_length - 1:
+            quotient = pos // arr_length
+            pos = pos - (quotient * arr_length)
+        elif pos < 0:
+            quotient = abs(pos // arr_length)
+            pos = pos + (quotient * arr_length)
+
+        if not 0 <= pos <= arr_length - 1 or flags[pos] == 1:
+            return False
+
+        flags[pos] += 1
+        winder -= 1
+
+    if sum(flags) == arr_length:
+        return True
+
+    return False
+
+
+array = [10, 11, -6, -23, -2, 3, 88, 908, -26]
+hasSingleCycle(array)
