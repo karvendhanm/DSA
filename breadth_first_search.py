4106a6208c4332ae6435a61b97c0329a69274255
karvendhanm <karvsmech@gmail.com>
2024-10-20 12:07:35 +0530

    first iteration O(n) time and O(n) space complexity

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -8,12 +8,44 @@ class Node:
         self.children.append(Node(name))
         return self
 
+
+    # first iteration
+    # O(n) time | O(n) space
     def breadthFirstSearch(self, array):
         # Write your code here.
-        pass
+        _arr = []
+        _arr.append(self)
+
+        while _arr:
+            obj = _arr.pop(0)
+            array.append(obj.name)
+            _arr.extend(obj.children)
+        return array
 
 
+E_node = Node('E')
+F_node = Node('F')
+F_node.addChild('I')
+F_node.addChild('J')
+B_node = Node('B')
+B_node.children.append(E_node)
+B_node.children.append(F_node)
+C_node = Node('C')
+G_node = Node('G')
+H_node = Node('H')
+G_node.addChild('K')
+H_node = Node('H')
+D_node = Node('D')
+D_node.children.append(G_node)
+D_node.children.append(H_node)
 rootNode = Node('A')
+rootNode.children.append(B_node)
+rootNode.children.append(C_node)
+rootNode.children.append(D_node)
+
+
+print(rootNode.breadthFirstSearch([]))
+
 
 
 
