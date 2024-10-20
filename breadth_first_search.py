a028f7e09c40f40e9e98870ebdc7ebf7c245d023
karvendhanm <karvsmech@gmail.com>
2024-10-20 11:36:35 +0530

    create breadth_first_search.py

---
+++ breadth_first_search.py
@@ -0,0 +1,20 @@
+# breadth-first search
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def breadthFirstSearch(self, array):
+        # Write your code here.
+        pass
+
+
+rootNode = Node('A')
+
+
+
+
