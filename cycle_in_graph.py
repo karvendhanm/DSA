f064281ebfef8dac5b224761ffac2bc48a0ea2f4
karvendhanm <karvsmech@gmail.com>
2024-11-05 17:54:36 +0530

    first iteration

---
+++ cycle_in_graph.py
@@ -0,0 +1,26 @@
+# first iteration
+# O(e * n) time | O(n) space
+# where n is the number of nodes/vertices in the graph
+# and e is the number of edges
+def cycleInGraph(edges):
+    # Write your code here.
+    # graph traversal problem
+    for _idx, edge in enumerate(edges):
+        stack = [*edge]
+        visited_nodes = []
+        while stack:
+            outbound_idx = stack.pop()
+            if outbound_idx == _idx:
+                return True
+            visited_nodes.append(outbound_idx)
+            new_additions = [elem for elem in edges[outbound_idx] if elem not in visited_nodes and elem not in stack]
+            stack.extend(new_additions)
+    return False
+
+
+edges = [
+    [1, 2],
+    [2],
+    [1]
+  ]
+cycleInGraph(edges)
