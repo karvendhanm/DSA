d301017e1f4f526a4919afae32fe47e24349fef4
karvendhanm <karvsmech@gmail.com>
2024-11-10 19:45:33 +0530

    third iteration - optimal time and space

--- cycle_in_graph.py
+++ cycle_in_graph.py
@@ -22,52 +22,92 @@
 # O(e + v) time | O(v) space
 # where v is the number of nodes/vertices in the graph
 # and e is the number of edges
+# def cycleInGraph(edges):
+#     # Write your code here.
+#     numVertices = len(edges)
+#     visitedNodes = [0 for _ in range(numVertices)]
+#     currentlyInStack = [0 for _ in range(numVertices)]
+#
+#     for node in range(numVertices):
+#         if visitedNodes[node]:
+#             continue
+#
+#         isCycleInNode = getIsCycleInGraph(node, edges, visitedNodes, currentlyInStack)
+#         if isCycleInNode:
+#             return True
+#
+#     return False
+#
+#
+# def getIsCycleInGraph(node, edges, visitedNodes, currentlyInStack):
+#     visitedNodes[node] = 1
+#     currentlyInStack[node] = 1
+#     outboundEdges = edges[node]
+#
+#     for outboundEdge in outboundEdges:
+#         if visitedNodes[outboundEdge] and currentlyInStack[outboundEdge]:
+#             # indicates the presence of a back edge (a cycle)
+#             return True
+#
+#         # this node has already been visited but not in stack
+#         if visitedNodes[outboundEdge]:
+#             continue
+#
+#         isCycleInNode = getIsCycleInGraph(outboundEdge, edges, visitedNodes, currentlyInStack)
+#         if isCycleInNode:
+#             return True
+#
+#     currentlyInStack[node] = 0
+#     return False
+
+
+# third iteration
+# O(e + v) time | O(v) space
+# where v is the number of nodes/vertices in the graph
+# and e is the number of edges
+
+# white indicates unvisited node
+# grey indicates visited node and in stack
+# black indicates that the node is finished (node has already been visited but not in stack).
+WHITE, GREY, BLACK = 0, 1, 2
+
+
 def cycleInGraph(edges):
-    # Write your code here.
     numVertices = len(edges)
-    visitedNodes = [0 for _ in range(numVertices)]
-    currentlyInStack = [0 for _ in range(numVertices)]
+    colors = [0 for _ in range(numVertices)]
 
     for node in range(numVertices):
-        if visitedNodes[node]:
+        if colors[node] != 0:
             continue
 
-        isCycleInNode = getIsCycleInGraph(node, edges, visitedNodes, currentlyInStack)
+        isCycleInNode = getIsCycleInGraph(node, edges, colors)
         if isCycleInNode:
             return True
-
     return False
 
 
-def getIsCycleInGraph(node, edges, visitedNodes, currentlyInStack):
-    visitedNodes[node] = 1
-    currentlyInStack[node] = 1
+def getIsCycleInGraph(node, edges, colors):
+    colors[node] = 1
     outboundEdges = edges[node]
 
     for outboundEdge in outboundEdges:
-        if visitedNodes[outboundEdge] and currentlyInStack[outboundEdge]:
-            # indicates the presence of a back edge (a cycle)
+        if colors[outboundEdge] == 2:
+            continue
+        elif colors[outboundEdge] == 1:
             return True
 
-        if visitedNodes[outboundEdge]:
-            continue
-
-        isCycleInNode = getIsCycleInGraph(outboundEdge, edges, visitedNodes, currentlyInStack)
+        isCycleInNode = getIsCycleInGraph(outboundEdge, edges, colors)
         if isCycleInNode:
             return True
-
-    currentlyInStack[node] = 0
+    colors[node] = 2
     return False
 
 
 edges = [
-    [1, 3],
-    [2, 3, 4],
-    [0],
-    [],
-    [2, 5],
+    [1, 2],
+    [2],
     []
-]
+  ]
 print(cycleInGraph(edges))
 
 #   [
