32fd1bd61a18392d3055e204ab4248ba60082184
karvendhanm <karvsmech@gmail.com>
2024-11-11 18:49:10 +0530

    task_assignment.py solved optimal time and space complexity

--- task_assignment.py
+++ task_assignment.py
@@ -1,8 +1,33 @@
+# O(nlog(n)) time and O(n) space
+# where n is the number of tasks.
+# O(nlog(n)) as we are sorting the array
 def taskAssignment(k, tasks):
     # Write your code here.
-    return []
+    durationToIndices = getDurationToIndices(tasks)
+    pairedTasks = []
+
+    sortedTasks = sorted(tasks)
+    for _idx in range(k):
+        firstElement = sortedTasks[_idx]
+        firstElementIndex = durationToIndices[firstElement].pop()   # we take the last element in the list
+
+        secondElement = sortedTasks[len(tasks) - 1 - _idx]
+        secondElementIndex = durationToIndices[secondElement].pop()
+        pairedTasks.append([firstElementIndex, secondElementIndex])
+
+    return pairedTasks
+
+
+def getDurationToIndices(tasks):
+    durationToIndices = {}
+    for _idx, elem in enumerate(tasks):
+        if elem in durationToIndices:
+            durationToIndices[elem].append(_idx)
+        else:
+            durationToIndices[elem] = [_idx]
+    return durationToIndices
 
 
 k = 3
 tasks = [1, 3, 5, 3, 1, 4]
-taskAssignment(k, tasks)
+print(taskAssignment(k, tasks))
