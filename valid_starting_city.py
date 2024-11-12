b69c8e023ad8480651730c60a4c83a37ba47d7db
karvendhanm <karvsmech@gmail.com>
2024-11-12 16:13:47 +0530

    valid_starting_city.py third iteration optimal time and space complexity

--- valid_starting_city.py
+++ valid_starting_city.py
@@ -22,22 +22,42 @@
 # second iteration
 # O(n^2) time and O(1) space
 # sub-optimal time complexity
+# def validStartingCity(distances, fuel, mpg):
+#     # Write your code here.
+#     mileage = list(map(lambda x: x * mpg, fuel))
+#     numCities = len(distances)
+#
+#     for _idx in range(numCities):
+#         cumSum = 0
+#         for _idx1, i in enumerate(range(_idx, numCities + _idx)):
+#             _idx2 = i % numCities
+#             cumSum += mileage[_idx2] - distances[_idx2]
+#             if cumSum < 0:
+#                 break
+#             elif cumSum == 0 and _idx1 == numCities - 1:
+#                 return _idx
+#     return -1
+
+
+# O(n) time and O(1) space
 def validStartingCity(distances, fuel, mpg):
-    # Write your code here.
-    mileage = list(map(lambda x: x * mpg, fuel))
     numCities = len(distances)
+    milesRemaining = 0
 
-    for _idx in range(numCities):
-        cumSum = 0
-        for _idx1, i in enumerate(range(_idx, numCities + _idx)):
-            _idx2 = i % numCities
-            cumSum += mileage[_idx2] - distances[_idx2]
-            if cumSum < 0:
-                break
-            elif cumSum == 0 and _idx1 == numCities - 1:
-                return _idx
-    return -1
+    indexOfStartCityCandidate = 0
+    milesRemainingAtStartCityCandidate = 0
 
+    for startCityIndex in range(1, numCities):
+        fuelFromPreviousCity = fuel[startCityIndex - 1]
+        distanceTravelledFromPreviousCity = distances[startCityIndex - 1]
+
+        milesRemaining += fuelFromPreviousCity * mpg - distanceTravelledFromPreviousCity
+
+        if milesRemaining < milesRemainingAtStartCityCandidate:
+            milesRemainingAtStartCityCandidate = milesRemaining
+            indexOfStartCityCandidate = startCityIndex
+
+    return indexOfStartCityCandidate
 
 
 distances = [5, 25, 15, 10, 15]
