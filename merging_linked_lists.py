7c265156af5b76ac9a66301f3cb2d8bf46d2de8c
Karvendhan M <karvsmech@gmail.com>
2024-11-19 15:58:22 +0530

    Update merging_linked_lists.py adding fourth iteration

--- merging_linked_lists.py
+++ merging_linked_lists.py
@@ -40,6 +40,59 @@ def mergingLinkedLists(linkedListOne, linkedListTwo):
     return None
 
 
+def getNumberOfNodes(linkedList):
+    count = 0
+    while linkedList is not None:
+        count += 1
+        linkedList = linkedList.next
+    return count
+
+# third iteration
+# O(n + m) time and O(1) space
+# where n is the number of nodes in linkedListOne
+# where m is the number of nodes in linkedListTwo
+def mergingLinkedLists(linkedListOne, linkedListTwo):
+    count1 = getNumberOfNodes(linkedListOne)
+    count2 = getNumberOfNodes(linkedListTwo)
+
+    biggerLinkedList = linkedListOne if count1 >= count2 else linkedListTwo
+    smallerLinkedList = linkedListTwo if count1 >= count2 else linkedListOne
+
+    diff = abs(count1 - count2)
+    while diff:
+        biggerLinkedList = biggerLinkedList.next
+        diff -= 1
+
+    while biggerLinkedList and smallerLinkedList:
+        if biggerLinkedList == smallerLinkedList:
+            return biggerLinkedList
+        biggerLinkedList = biggerLinkedList.next
+        smallerLinkedList = smallerLinkedList.next
+    return None
+
+
+# fourth iteration
+# O(n + m) time and O(1) space
+# where n is the number of nodes in linkedListOne
+# where m is the number of nodes in linkedListTwo
+def mergingLinkedLists(linkedListOne, linkedListTwo):
+
+    dummyNodeOne = linkedListOne
+    dummyNodeTwo = linkedListTwo
+    while dummyNodeOne is not dummyNodeTwo:
+        if not dummyNodeOne:
+            dummyNodeOne = linkedListTwo
+        else:
+            dummyNodeOne = dummyNodeOne.next
+
+        if not dummyNodeTwo:
+            dummyNodeTwo = linkedListOne
+        else:
+            dummyNodeTwo = dummyNodeTwo.next
+    return dummyNodeOne
+
+
+
 node11 = LinkedList(2)
 node12 = LinkedList(4)
 node13 = LinkedList(7)
@@ -50,7 +103,6 @@ node12.next = node13
 node13.next = node14
 
 node21 = LinkedList(9)
-node21.next = node13
 
 tmp = mergingLinkedLists(node11, node21)
 
