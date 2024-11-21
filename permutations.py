11c1353fd08067c8c999923588ee0b7ff7f47a31
Karvendhan M <karvsmech@gmail.com>
2024-11-21 15:31:07 +0530

    Create permutations.py first iteration

---
+++ permutations.py
@@ -0,0 +1,21 @@
+# O(n! * n^2) time | O(n! * n) space
+def getPermutations(array):
+    # Write your code here.
+    permutations = []
+    getPermutationsHelper(array, [], permutations)
+    return permutations
+
+
+def getPermutationsHelper(array, permutation, permutations):
+    if not array:
+        permutations.append(permutation)
+        return
+    else:
+        for i in range(len(array)):
+            newArr = array[:i] + array[i+1:]
+            newPermutation = permutation + [array[i]]
+            getPermutationsHelper(newArr, newPermutation, permutations)
+
+
+array = [1, 2, 3]
+print(getPermutations(array))
