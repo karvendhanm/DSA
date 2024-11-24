024a65b034f3e35275387bc51f2ad50213ee292a
karvendhanm <karvsmech@gmail.com>
2024-11-24 19:07:33 +0530

    third iteration another recursive solution

--- powerset.py
+++ powerset.py
@@ -14,12 +14,28 @@
 
 
 # O(n * 2^n) time | O(n * 2^n) space
-def powerset(array):
-    # iterative solution
-    subsets = [[]]
-    for elem in array:
-        for i in range(len(subsets)):
-            subsets.append(subsets[i] + [elem])
+# def powerset(array):
+#     # iterative solution
+#     subsets = [[]]
+#     for elem in array:
+#         for i in range(len(subsets)):
+#             subsets.append(subsets[i] + [elem])
+#     return subsets
+
+
+# O(n * 2^n) time | O(n * 2^n) space
+# Another recursive solution
+# this recursive solution is based on the idea that
+# P([1, 2]) = P([1]) + [2] added to all the subsets.
+def powerset(array, idx=None):
+    if idx is None:
+        idx = len(array) - 1
+    elif idx < 0:
+        return [[]]
+    ele = array[idx]
+    subsets = powerset(array, idx-1)
+    for i in range(len(subsets)):
+        subsets.append(subsets[i] + [ele])
     return subsets
 
 
