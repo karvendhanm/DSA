0e3f89497132688122c54e26ed881dd71d548d75
karvendhanm <karvsmech@gmail.com>
2024-11-24 11:44:45 +0530

    renaming variables

--- powerset.py
+++ powerset.py
@@ -1,16 +1,16 @@
 # O(n * 2^n) time | O(n * 2^n) space
 def powerset(array):
-    pow = []
-    powersetHelper(array, [], pow)
-    return pow
+    powerset = []
+    powersetHelper(array, [], powerset)
+    return powerset
 
 
-def powersetHelper(array, currentPermutation, permutations):
-    permutations.append(currentPermutation)
+def powersetHelper(array, currentPowerset, powerset):
+    powerset.append(currentPowerset)
     for i in range(len(array)):
         newArr = array[i + 1:]
-        newPermutation = currentPermutation + [array[i]]
-        powersetHelper(newArr, newPermutation, permutations)
+        newPowerset = currentPowerset + [array[i]]
+        powersetHelper(newArr, newPowerset, powerset)
 
 
 array = [1, 2, 3]
