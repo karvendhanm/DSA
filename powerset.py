89cb029336c2877c030b8037d710daa0d13c70b6
karvendhanm <karvsmech@gmail.com>
2024-11-24 18:31:57 +0530

    powerset.py second iteration iterative solution

--- powerset.py
+++ powerset.py
@@ -1,16 +1,26 @@
 # O(n * 2^n) time | O(n * 2^n) space
+# def powerset(array):
+#     powerset = []
+#     powersetHelper(array, [], powerset)
+#     return powerset
+#
+#
+# def powersetHelper(array, currentPowerset, powerset):
+#     powerset.append(currentPowerset)
+#     for i in range(len(array)):
+#         newArr = array[i + 1:]
+#         newPowerset = currentPowerset + [array[i]]
+#         powersetHelper(newArr, newPowerset, powerset)
+
+
+# O(n * 2^n) time | O(n * 2^n) space
 def powerset(array):
-    powerset = []
-    powersetHelper(array, [], powerset)
-    return powerset
-
-
-def powersetHelper(array, currentPowerset, powerset):
-    powerset.append(currentPowerset)
-    for i in range(len(array)):
-        newArr = array[i + 1:]
-        newPowerset = currentPowerset + [array[i]]
-        powersetHelper(newArr, newPowerset, powerset)
+    # iterative solution
+    subsets = [[]]
+    for elem in array:
+        for i in range(len(subsets)):
+            subsets.append(subsets[i] + [elem])
+    return subsets
 
 
 array = [1, 2, 3]
