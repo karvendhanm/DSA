288a8dba6638e78e9f6ca4194b1ad9c955de5722
Karvendhan M <karvsmech@gmail.com>
2024-11-25 14:40:21 +0530

    Create phone_number_mnemonics.py recursive and iterative solutions

---
+++ phone_number_mnemonics.py
@@ -0,0 +1,51 @@
+integer2ButtonLetters = {
+    '0': ['0'],
+    '1': ['1'],
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z']
+}
+
+
+# first iteration
+# recursive solution
+# O(n * 4 ^ n) time | O(n * 4 ^ n) space
+# def phoneNumberMnemonics(phoneNumber, idx=None):
+#     # Write your code here.
+#     if idx is None:
+#         idx = len(phoneNumber) - 1
+#     if idx < 0:
+#         return [""]
+#     phoneNumberCharacters = list(phoneNumber)
+#     charAtIndex = phoneNumberCharacters[idx]
+#     mnemonics = phoneNumberMnemonics(phoneNumber, idx - 1)
+#     buttonLetters = integer2ButtonLetters.get(charAtIndex)
+#     currentMnemonics = []
+#     for mnemonic in mnemonics:
+#         for letter in buttonLetters:
+#             currentMnemonics.append(mnemonic + letter)
+#     return currentMnemonics
+
+
+# second iteration
+# iterative solution
+# O(n * 4 ^ n) time | O(n * 4 ^ n) space
+def phoneNumberMnemonics(phoneNumber):
+    phoneNumberCharacters = list(phoneNumber)
+    currentMnemonics = [""]
+    for character in phoneNumberCharacters:
+        mnemonics = currentMnemonics
+        currentMnemonics = []
+        for mnemonic in mnemonics:
+            for letter in integer2ButtonLetters.get(character):
+                currentMnemonics.append(mnemonic + letter)
+    return currentMnemonics
+
+
+phoneNumber = "7777"
+print(phoneNumberMnemonics(phoneNumber))
