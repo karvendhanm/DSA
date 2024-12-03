eea48f470e0f2f43e2c0e286466d38e4a9ffd123
karvendhanm <karvsmech@gmail.com>
2024-12-03 18:52:21 +0530

    create reveal_minesweeper.py

---
+++ reveal_minesweeper.py
@@ -0,0 +1,14 @@
+def revealMinesweeper(board, row, column):
+    # Write your code here.
+    return []
+
+
+board = [
+    ["H", "H", "H", "H", "M"],
+    ["H", "H", "M", "H", "H"],
+    ["H", "H", "H", "H", "H"],
+    ["H", "H", "H", "H", "H"]
+]
+row = 3
+column = 0
+revealMinesweeper(board, row, column)
