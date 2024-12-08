ce93762aee9e145fd124885bb16394d1e4a5be81
karvendhanm <karvsmech@gmail.com>
2024-12-08 07:17:14 +0530

    another optimal time and space complexity method

--- three_number_sort_v4.py
+++ three_number_sort_v4.py
@@ -1,18 +1,38 @@
 # three number sort using bucket sort.
 # O(n) time | O(1) space
+# def threeNumberSort(array, order):
+#     _dict = {}
+#     for elem in order:
+#         _dict[elem] = 0
+#
+#     for elem in array:
+#         _dict[elem] += 1
+#
+#     _idx = 0
+#     for i in order:
+#         for _ in range(_dict[i]):
+#             array[_idx] = i
+#             _idx += 1
+#     return array
+
+
+def swap(array, firstIdx, secondIdx):
+    array[firstIdx], array[secondIdx] = array[secondIdx], array[firstIdx]
+    return
+
+
+# O(n) time | O(1) space
 def threeNumberSort(array, order):
-    _dict = {}
-    for elem in order:
-        _dict[elem] = 0
-
-    for elem in array:
-        _dict[elem] += 1
-
-    _idx = 0
-    for i in order:
-        for _ in range(_dict[i]):
-            array[_idx] = i
-            _idx += 1
+    first_idx = 0
+    for current_idx in range(len(array)):
+        if array[current_idx] == order[0]:
+            swap(array, current_idx, first_idx)
+            first_idx += 1
+    last_idx = len(array) - 1
+    for current_idx in reversed(range(len(array))):
+        if array[current_idx] == order[2]:
+            swap(array, current_idx, last_idx)
+            last_idx -= 1
     return array
 
 
