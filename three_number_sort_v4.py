0905f7cf4a0cdb6017a7a46d5023a41fd9dd5e94
karvendhanm <karvsmech@gmail.com>
2024-12-08 08:06:42 +0530

    most optimal solution with a single pass

--- three_number_sort_v4.py
+++ three_number_sort_v4.py
@@ -22,17 +22,39 @@ def swap(array, firstIdx, secondIdx):
 
 
 # O(n) time | O(1) space
+# def threeNumberSort(array, order):
+#     first_idx = 0
+#     for current_idx in range(len(array)):
+#         if array[current_idx] == order[0]:
+#             swap(array, current_idx, first_idx)
+#             first_idx += 1
+#     last_idx = len(array) - 1
+#     for current_idx in reversed(range(len(array))):
+#         if array[current_idx] == order[2]:
+#             swap(array, current_idx, last_idx)
+#             last_idx -= 1
+#     return array
+
+
+# O(n) time | O(1) space
+# the cleanest and most optimal solution
+# this is the most optimal solution as this solution makes just one pass
+# at the array, when compared to 2 passes by the other 2 solutions
 def threeNumberSort(array, order):
-    first_idx = 0
-    for current_idx in range(len(array)):
-        if array[current_idx] == order[0]:
-            swap(array, current_idx, first_idx)
-            first_idx += 1
-    last_idx = len(array) - 1
-    for current_idx in reversed(range(len(array))):
-        if array[current_idx] == order[2]:
-            swap(array, current_idx, last_idx)
-            last_idx -= 1
+    first = second = idx = 0
+    last = len(array) - 1
+    while second <= last:
+        if array[idx] == order[0]:
+            swap(array, idx, first)
+            first += 1
+            second += 1
+            idx += 1
+        elif array[idx] == order[1]:
+            second += 1
+            idx += 1
+        elif array[idx] == order[2]:
+            swap(array, idx, last)
+            last -= 1
     return array
 
 
