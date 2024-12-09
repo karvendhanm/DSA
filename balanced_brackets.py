d7943d1d6d8b22e32ac03abf5ef970003ca9e0fc
Karvendhan M <karvsmech@gmail.com>
2024-12-09 16:30:44 +0530

    Update balanced_brackets.py further refactoring the code

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -30,29 +30,50 @@
 
 # second iteration
 # O(n) time | O(n) space
+# def balancedBrackets(string):
+#     # Write your code here.
+#     stack = []
+#     for bracket in list(string):
+#         if bracket == "(":
+#             stack.append('A')
+#         elif bracket == "{":
+#             stack.append('B')
+#         elif bracket == "[":
+#             stack.append('C')
+#         elif bracket == ")":
+#             if not stack or stack.pop() != 'A':
+#                 return False
+#         elif bracket == "}":
+#             if not stack or stack.pop() != 'B':
+#                 return False
+#         elif bracket == "]":
+#             if not stack or stack.pop() != 'C':
+#                 return False
+#     if not stack:
+#         return True
+#     return False
+
+
+# third iteration.
+# O(n) time | O(n) space.
+# refactoring the code further.
 def balancedBrackets(string):
     # Write your code here.
-    _lst = []
-    string = list(string)
-    for bracket in string:
-        if bracket == "(":
-            _lst.append('A')
-        elif bracket == "{":
-            _lst.append('B')
-        elif bracket == "[":
-            _lst.append('C')
-        elif bracket == ")":
-            if not _lst or _lst.pop() != 'A':
+    closingBrackets = ")]}"
+    openingBrackets = "([{"
+    matchingBrackets = {
+        ")": "(",
+        "]": "[",
+        "}": "{"
+    }
+    stack = []
+    for char in string:
+        if char in openingBrackets:
+            stack.append(char)
+        elif char in closingBrackets:
+            if not stack or stack.pop() != matchingBrackets[char]:
                 return False
-        elif bracket == "}":
-            if not _lst or _lst.pop() != 'B':
-                return False
-        elif bracket == "]":
-            if not _lst or _lst.pop() != 'C':
-                return False
-    if not _lst:
-        return True
-    return False
+    return len(stack) == 0
 
 
 string = "([])(){}(())()()"
