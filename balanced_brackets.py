1482d96db1db3ac2a4badd3cc298a333477cc0a5
Karvendhan M <karvsmech@gmail.com>
2024-12-09 16:01:56 +0530

    Update balanced_brackets.py second iteration  O(n) time complexity

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -1,29 +1,56 @@
 # first iteration
 # O(n ^ 2) time | O(n) space
+# def balancedBrackets(string):
+#     # Write your code here.
+#     brackets = ""
+#     string = list(string)
+#     for bracket in string:
+#         if bracket == "(":
+#             brackets = brackets + 'A'
+#         elif bracket == "{":
+#             brackets = brackets + 'B'
+#         elif bracket == "[":
+#             brackets = brackets + 'C'
+#         elif bracket == ")":
+#             if brackets == "" or brackets[-1] != 'A':
+#                 return False
+#             brackets = brackets[:-1]
+#         elif bracket == "}":
+#             if brackets == "" or brackets[-1] != 'B':
+#                 return False
+#             brackets = brackets[:-1]
+#         elif bracket == "]":
+#             if brackets == "" or brackets[-1] != 'C':
+#                 return False
+#             brackets = brackets[:-1]
+#     if brackets == "":
+#         return True
+#     return False
+
+
+# second iteration
+# O(n) time | O(n) space
 def balancedBrackets(string):
     # Write your code here.
-    brackets = ""
+    _lst = []
     string = list(string)
     for bracket in string:
         if bracket == "(":
-            brackets = brackets + 'A'
+            _lst.append('A')
         elif bracket == "{":
-            brackets = brackets + 'B'
+            _lst.append('B')
         elif bracket == "[":
-            brackets = brackets + 'C'
+            _lst.append('C')
         elif bracket == ")":
-            if brackets == "" or brackets[-1] != 'A':
+            if not _lst or _lst.pop() != 'A':
                 return False
-            brackets = brackets[:-1]
         elif bracket == "}":
-            if brackets == "" or brackets[-1] != 'B':
+            if not _lst or _lst.pop() != 'B':
                 return False
-            brackets = brackets[:-1]
         elif bracket == "]":
-            if brackets == "" or brackets[-1] != 'C':
+            if not _lst or _lst.pop() != 'C':
                 return False
-            brackets = brackets[:-1]
-    if brackets == "":
+    if not _lst:
         return True
     return False
 
