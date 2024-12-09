7927239e4dd57cb938475a177ff3508ca77b6954
karvendhanm <karvsmech@gmail.com>
2024-12-10 04:24:58 +0530

    second iteration - optimal time complexity

--- sunset_views.py
+++ sunset_views.py
@@ -1,33 +1,47 @@
 # first iteration
 # O(n ^ 2) time | O(n) space
 # sub-optimal time complexity
+# def sunsetViews(buildings, direction):
+#     # Write your code here.
+#     if not buildings:
+#         return []
+#
+#     indices = []
+#     _range = None
+#     if direction == "EAST":
+#         for currentBuildingIdx in range(len(buildings) - 1):
+#             for nextBuildingIdx in range(currentBuildingIdx + 1, len(buildings)):
+#                 if buildings[currentBuildingIdx] <= buildings[nextBuildingIdx]:
+#                     break
+#                 if nextBuildingIdx == len(buildings) - 1:
+#                     indices.append(currentBuildingIdx)
+#         indices.append(len(buildings) - 1)
+#     else:
+#         for currentBuildingIdx in reversed(range(1, len(buildings))):
+#             for nextBuildingIdx in range(currentBuildingIdx-1, -1, -1):
+#                 if buildings[currentBuildingIdx] <= buildings[nextBuildingIdx]:
+#                     break
+#                 if nextBuildingIdx == 0:
+#                     indices.append(currentBuildingIdx)
+#         indices.append(0)
+#
+#     return sorted(indices)  # O(n log (n)) time complexity
+
+
+# second iteration
+# O(n) time | O(n) space
 def sunsetViews(buildings, direction):
-    # Write your code here.
-    if not buildings:
-        return []
-
+    maxHeight = 0
     indices = []
-    _range = None
-    if direction == "EAST":
-        for currentBuildingIdx in range(len(buildings) - 1):
-            for nextBuildingIdx in range(currentBuildingIdx + 1, len(buildings)):
-                if buildings[currentBuildingIdx] <= buildings[nextBuildingIdx]:
-                    break
-                if nextBuildingIdx == len(buildings) - 1:
-                    indices.append(currentBuildingIdx)
-        indices.append(len(buildings) - 1)
-    else:
-        for currentBuildingIdx in reversed(range(1, len(buildings))):
-            for nextBuildingIdx in range(currentBuildingIdx-1, -1, -1):
-                if buildings[currentBuildingIdx] <= buildings[nextBuildingIdx]:
-                    break
-                if nextBuildingIdx == 0:
-                    indices.append(currentBuildingIdx)
-        indices.append(0)
+    _range = range(len(buildings)) if direction == 'WEST' else range(len(buildings) - 1, -1, -1)
 
-    return sorted(indices)  # O(n log (n)) time complexity
+    for buildingIdx in _range:
+        if buildings[buildingIdx] > maxHeight:
+            indices.append(buildingIdx)
+            maxHeight = buildings[buildingIdx]
+    return indices if direction == 'WEST' else indices[::-1]
 
 
 buildings = [3, 5, 4, 4, 3, 1, 3, 2]
-direction = 'EAST'
+direction = 'WEST'
 print(sunsetViews(buildings, direction))
