a63734ce31ea11da98f5d485214839c7421382d3
karvendhanm <karvsmech@gmail.com>
2024-12-16 19:02:16 +0530

    complete implementation

--- sort_stack.py
+++ sort_stack.py
@@ -1,7 +1,26 @@
+# O(n^2) time | O(n) space
 def sortStack(stack):
     # Write your code here.
-    pass
+    return sortStackHelper(stack)
+
+
+def sortStackHelper(stack):
+    if not stack:
+        return stack
+
+    topElem = stack.pop()
+    sortStackHelper(stack)
+
+    biggerElements = []
+    while stack and stack[-1] > topElem:
+        biggerElements.append(stack.pop())
+    biggerElements.append(topElem)
+
+    while biggerElements:
+        stack.append(biggerElements.pop())
+
+    return stack
 
 
 stack = [-5, 2, -2, 4, 3, 1]
-sortStack(stack)
+print(sortStack(stack))
