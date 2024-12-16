7068c25062ae1f8fc893a3f45cc618602b08e059
karvendhanm <karvsmech@gmail.com>
2024-12-16 19:25:40 +0530

    second iteration - implemented without using any auxillary space

--- sort_stack.py
+++ sort_stack.py
@@ -1,24 +1,52 @@
+# first iteration
+# this solution is wrong as we are using
+# an auxillary data structure biggerElements.
+# O(n^2) time | O(n) space
+# def sortStack(stack):
+#     # Write your code here.
+#     return sortStackHelper(stack)
+#
+#
+# def sortStackHelper(stack):
+#     if not stack:
+#         return stack
+#
+#     topElem = stack.pop()
+#     sortStackHelper(stack)
+#
+#     biggerElements = []
+#     while stack and stack[-1] > topElem:
+#         biggerElements.append(stack.pop())
+#     biggerElements.append(topElem)
+#
+#     while biggerElements:
+#         stack.append(biggerElements.pop())
+#
+#     return stack
+
+
+def insertElem(stack, topElem):
+    if not stack or stack[-1] < topElem:
+        stack.append(topElem)
+        return stack
+
+    currentTopElem = stack.pop()
+    insertElem(stack, topElem)
+    stack.append(currentTopElem)
+    return stack
+
+
+# second iteration
 # O(n^2) time | O(n) space
 def sortStack(stack):
     # Write your code here.
-    return sortStackHelper(stack)
-
-
-def sortStackHelper(stack):
     if not stack:
         return stack
 
     topElem = stack.pop()
-    sortStackHelper(stack)
+    sortStack(stack)
 
-    biggerElements = []
-    while stack and stack[-1] > topElem:
-        biggerElements.append(stack.pop())
-    biggerElements.append(topElem)
-
-    while biggerElements:
-        stack.append(biggerElements.pop())
-
+    insertElem(stack, topElem)
     return stack
 
 
