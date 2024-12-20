1579961592bcb0467e2dddd7a391ed4c8aea05d4
karvendhanm <karvsmech@gmail.com>
2024-12-20 19:20:48 +0530

    minor refactoring

--- group_anagrams.py
+++ group_anagrams.py
@@ -1,17 +1,17 @@
 # first iteration
 # O(n * m log(m)) time | O(n * m) space
 # where n is the number of strings in the array
-# where m is the maximum number of characters in a string
+# where m is the length of the longest string
 def groupAnagrams(words):
     # Write your code here.
-    _dict = {}
-    sortedWords = {word: "".join(sorted(word)) for word in words}
-    for key, value in sortedWords.items():
-        if value in _dict:
-            _dict[value].append(key)
+    anagrams = {}
+    for word in words:
+        sortedWord = "".join(sorted(word))
+        if sortedWord in anagrams:
+            anagrams[sortedWord].append(word)
         else:
-            _dict[value] = [key]
-    return [value for value in _dict.values()]
+            anagrams[sortedWord] = [word]
+    return list(anagrams.values())
 
 
 words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
