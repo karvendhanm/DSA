7d3417a48932f8097a9af3f98d8eba8e2b286e38
Karvendhan M <karvsmech@gmail.com>
2024-12-31 12:18:18 +0530

    Update min_rewards_v2.py fourth iteration - finding valleys - optimal time and space complexity

--- min_rewards_v2.py
+++ min_rewards_v2.py
@@ -1,4 +1,4 @@
-# second iteration - clever solution (optimal time and space complexity)
+# second iteration
 # O(n) time and O(n) space
 # def minRewards(scores):
 #     if len(scores) == 1:
@@ -18,23 +18,61 @@
 #     return sum(rewardsList)
 
 
-# third iteration - naive solution (sub-optimal time complexity)
+# third iteration
 # O(n ^ 2) time and O(n) space
+# def minRewards(scores):
+#     if len(scores) == 1:
+#         return 1
+#
+#     rewardsList = [1] * len(scores)
+#     for idx in range(1, len(scores)):
+#         if scores[idx] > scores[idx - 1]:
+#             rewardsList[idx] = max(rewardsList[idx], rewardsList[idx - 1] + 1)
+#         else:
+#             while idx > 0 and scores[idx] < scores[idx - 1]:
+#                 rewardsList[idx - 1] = max(rewardsList[idx - 1], rewardsList[idx] + 1)
+#                 idx -= 1
+#     return sum(rewardsList)
+
+
+# fourth iteration
+# O(n) time and O(n) space
+# the method where we find peaks and valleys.
 def minRewards(scores):
     if len(scores) == 1:
         return 1
 
     rewardsList = [1] * len(scores)
-    for idx in range(1, len(scores)):
-        if scores[idx] > scores[idx - 1]:
-            rewardsList[idx] = max(rewardsList[idx - 1] + 1, rewardsList[idx])
-        else:
-            while idx > 0 and scores[idx] < scores[idx - 1]:
-                rewardsList[idx - 1] = max(rewardsList[idx - 1], rewardsList[idx] + 1)
-                idx -= 1
+    for idx in range(len(scores)):
+        if isValley(idx, scores):
+            left = idx - 1
+            right = idx + 1
+
+            nextScore = scores[idx]
+            while left >= 0 and scores[left] > nextScore:
+                rewardsList[left] = max(rewardsList[left], rewardsList[left + 1] + 1)
+                nextScore = scores[left]
+                left -= 1
+
+            previousScore = scores[idx]
+            while right < len(scores) and scores[right] > previousScore:
+                rewardsList[right] = max(rewardsList[right], rewardsList[right - 1] + 1)
+                previousScore = scores[right]
+                right += 1
+
     return sum(rewardsList)
+
+
+def isValley(idx, scores):
+    if idx == 0 and scores[idx] < scores[idx + 1]:
+        return True
+    elif idx == len(scores) - 1 and scores[idx] < scores[idx - 1]:
+        return True
+
+    if scores[idx] < scores[idx - 1] and scores[idx] < scores[idx + 1]:
+        return True
+    return False
 
 
 scores = [8, 4, 2, 1, 3, 6, 7, 9, 5]
 print(minRewards(scores))
-
