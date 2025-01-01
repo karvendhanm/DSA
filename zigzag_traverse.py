85ce173207a2b05ac2e1232016a2566b4f219c11
Karvendhan M <karvsmech@gmail.com>
2025-01-01 12:04:14 +0530

    Update zigzag_traverse.py second iteration using only two directions up and down

--- zigzag_traverse.py
+++ zigzag_traverse.py
@@ -58,6 +58,47 @@ def zigzagTraverse(array):
     return resultArray
 
 
+# O(n) time | O(n) space
+# where n is the total number of elements
+# in the 2-dimensional array (height (h) * width (w))
+def zigzagTraverse(array):
+    rowIdx = columnIdx = 0
+    zigzagArray = [array[rowIdx][columnIdx]]
+    direction = 'down'
+    while not (rowIdx == len(array)-1 and columnIdx == len(array[0])-1):
+        if direction == 'down':
+            direction = 'up'
+            while rowIdx + 1 < len(array) and columnIdx - 1 >= 0:
+                rowIdx = rowIdx + 1
+                columnIdx = columnIdx - 1
+                zigzagArray.append(array[rowIdx][columnIdx])
+            # 2 special cases needs to be handled here for 'down' direction
+            # case 1: when the indexes are at the 0th column.
+            # case 2: when the indexes are at the last row.
+            if rowIdx == len(array)-1 and columnIdx + 1 < len(array[0]):
+                columnIdx = columnIdx + 1
+                zigzagArray.append(array[rowIdx][columnIdx])
+            elif columnIdx == 0 and rowIdx < len(array):
+                rowIdx = rowIdx + 1
+                zigzagArray.append(array[rowIdx][columnIdx])
+        elif direction == 'up':
+            direction = 'down'
+            while rowIdx - 1 >= 0 and columnIdx + 1 < len(array[0]):
+                rowIdx = rowIdx - 1
+                columnIdx = columnIdx + 1
+                zigzagArray.append(array[rowIdx][columnIdx])
+            # 2 special cases needs to be handled here for 'up' direction
+            # case 1: when the indexes are at the 0th row.
+            # case 2: when the indexes are at the last column.
+            if columnIdx == len(array[0])-1 and rowIdx + 1 < len(array):
+                rowIdx = rowIdx + 1
+                zigzagArray.append(array[rowIdx][columnIdx])
+            elif rowIdx == 0 and columnIdx + 1 < len(array[0]):
+                columnIdx = columnIdx + 1
+                zigzagArray.append(array[rowIdx][columnIdx])
+    return zigzagArray
+
+
 array = [
     [1, 3, 4, 10],
     [2, 5, 9, 11],
