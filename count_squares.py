09696c714f32b88cee65d39b76443652cd8f5ab6
karvendhanm <karvsmech@gmail.com>
2025-01-10 19:14:14 +0530

    create count_squares.py

---
+++ count_squares.py
@@ -0,0 +1,15 @@
+def countSquares(points):
+    # Write your code here.
+    return -1
+
+
+points = [
+    [1, 1],
+    [0, 0],
+    [-4, 2],
+    [-2, -1],
+    [0, 1],
+    [1, 0],
+    [-1, 4]
+  ]
+print(countSquares(points))
