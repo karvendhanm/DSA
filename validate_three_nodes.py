862521431a466d7fa8e252e01964b7d8fa60978f
Karvendhan M <karvsmech@gmail.com>
2025-01-16 15:30:00 +0530

    Update validate_three_nodes.py optimal time and space complexity

--- validate_three_nodes.py
+++ validate_three_nodes.py
@@ -28,23 +28,52 @@ nodeTwo = root.left
 nodeThree = root.left.right.left
 
 
+# def isParentNode(parentNode, childNode):
+#     if parentNode is None or childNode is None:
+#         return False
+
+#     if parentNode == childNode:
+#         return True
+#     return isParentNode(parentNode.left, childNode) or isParentNode(parentNode.right, childNode)
+
+
+# # first iteration
+# # O(n) time | O(n) space
+# # where n is the number of nodes of the BST
+# def validateThreeNodes(nodeOne, nodeTwo, nodeThree):
+#     # Write your code here.
+#     if isParentNode(nodeOne, nodeTwo):
+#         if isParentNode(nodeTwo, nodeThree):
+#             return True
+#     elif isParentNode(nodeThree, nodeTwo):
+#         if isParentNode(nodeTwo, nodeOne):
+#             return True
+#     return False
+
+
 def isParentNode(parentNode, childNode):
     if parentNode is None or childNode is None:
         return False
 
-    if parentNode == childNode:
-        return True
-    return isParentNode(parentNode.left, childNode) or isParentNode(parentNode.right, childNode)
+    while parentNode is not None:
+        if parentNode == childNode:
+            return True
+        elif childNode.value < parentNode.value:
+            parentNode = parentNode.left
+        elif childNode.value >= parentNode.value:
+            parentNode = parentNode.right
+    return False
 
 
+# second iteration
+# O(d) time | O(1) space
+# where d is the maximum depth of the BST
 def validateThreeNodes(nodeOne, nodeTwo, nodeThree):
     # Write your code here.
-    if isParentNode(nodeOne, nodeTwo):
-        if isParentNode(nodeTwo, nodeThree):
-            return True
-    elif isParentNode(nodeThree, nodeTwo):
-        if isParentNode(nodeTwo, nodeOne):
-            return True
+    if isParentNode(nodeOne, nodeTwo) and isParentNode(nodeTwo, nodeThree):
+        return True
+    elif isParentNode(nodeThree, nodeTwo) and isParentNode(nodeTwo, nodeOne):
+        return True
     return False
 
 
