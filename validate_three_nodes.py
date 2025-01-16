42d6be70a6a23ccb68d6d0309f286dd42293cd98
Karvendhan M <karvsmech@gmail.com>
2025-01-16 17:10:51 +0530

    Update validate_three_nodes.py - best time complexity

--- validate_three_nodes.py
+++ validate_three_nodes.py
@@ -65,22 +65,48 @@ nodeThree = root.left.right.left
 #     return False
 
 
+# def isParentNode(parentNode, childNode):
+#     while parentNode is not None and parentNode is not childNode:
+#         parentNode = parentNode.left if childNode.value < parentNode.value else parentNode.right
+#     return parentNode is childNode
+
+
+# # second iteration
+# # O(d) time | O(1) space
+# # where d is the maximum depth of the BST
+# def validateThreeNodes(nodeOne, nodeTwo, nodeThree):
+#     # Write your code here.
+#     if isParentNode(nodeOne, nodeTwo) and isParentNode(nodeTwo, nodeThree):
+#         return True
+#     elif isParentNode(nodeThree, nodeTwo) and isParentNode(nodeTwo, nodeOne):
+#         return True
+#     return False
+
+
 def isParentNode(parentNode, childNode):
     while parentNode is not None and parentNode is not childNode:
         parentNode = parentNode.left if childNode.value < parentNode.value else parentNode.right
     return parentNode is childNode
 
 
-# second iteration
+# third iteration
 # O(d) time | O(1) space
-# where d is the maximum depth of the BST
+# where d is the distance between nodeOne and nodeThree
 def validateThreeNodes(nodeOne, nodeTwo, nodeThree):
     # Write your code here.
-    if isParentNode(nodeOne, nodeTwo) and isParentNode(nodeTwo, nodeThree):
-        return True
-    elif isParentNode(nodeThree, nodeTwo) and isParentNode(nodeTwo, nodeOne):
-        return True
-    return False
+    search1, search2 = nodeOne, nodeThree
+    while (search1 != search2 and
+           (search1 is not None or search2 is not None) and
+           (search1 is not nodeTwo and search2 is not nodeTwo)):
+        if search1 is not None:
+            search1 = search1.left if nodeTwo.value < search1.value else search1.right
+        if search2 is not None:
+            search2 = search2.left if nodeTwo.value < search2.value else search2.right
+
+    if (search1 is None and search2 is None) or search1 is search2:
+        return False
+
+    return isParentNode(nodeTwo, nodeThree if search1 is nodeTwo else nodeOne)
 
 
 print(validateThreeNodes(nodeOne, nodeTwo, nodeThree))
