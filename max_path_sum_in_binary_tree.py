e47df677dd31fc5a0ed1c53011075b016175f76d
karvendhanm <karvsmech@gmail.com>
2025-01-23 18:45:44 +0530

    maximum path sum in a binary tree

---
+++ max_path_sum_in_binary_tree.py
@@ -0,0 +1,43 @@
+class BT:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+root = BT(1)
+
+# left branch
+root.left = BT(2)
+root.left.left = BT(4)
+root.left.right = BT(5)
+
+# right branch
+root.right = BT(3)
+root.right.right = BT(7)
+root.right.left = BT(6)
+
+
+# O(n) time | O(log(n)) space
+def maxPathSum(tree):
+    _, maxPathSum = maxPathSumHelper(tree)
+    return maxPathSum
+
+
+def maxPathSumHelper(tree):
+    # Write your code here.
+    if tree is None:
+        return 0, float('-inf')
+
+    leftMaxSumAsBranch, leftMaxPathSum = maxPathSumHelper(tree.left)
+    rightMaxSumAsBranch, rightMaxPathSum = maxPathSumHelper(tree.right)
+    maxChildSumAsBranch = max(leftMaxSumAsBranch, rightMaxSumAsBranch)
+
+    value = tree.value
+    maxSumAsBranch = max(maxChildSumAsBranch + value, value)
+    maxSumAsRootNode = max(maxSumAsBranch, leftMaxSumAsBranch + value + rightMaxSumAsBranch)
+    maxPathSum = max(leftMaxPathSum, rightMaxPathSum, maxSumAsRootNode)
+    return maxSumAsBranch, maxPathSum
+
+
+print(maxPathSum(root))
