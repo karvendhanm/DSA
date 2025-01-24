6ddc9808a935366daeedc2945b0b50dff0364d5c
karvendhanm <karvsmech@gmail.com>
2025-01-24 18:55:18 +0530

    assigning parent node to all the nodes

--- find_nodes_distance_k.py
+++ find_nodes_distance_k.py
@@ -1,8 +1,10 @@
 class BT:
-    def __init__(self, value, left=None, right=None):
+    def __init__(self, value, left=None, right=None, parent=None):
         self.value = value
         self.left = left
         self.right = right
+        self.parent = parent
+
 
 
 tree = BT(1)
@@ -21,7 +23,17 @@ tree.right.right.right = BT(8)
 
 def findNodesDistanceK(tree, target, k):
     # Write your code here.
-    return []
+    assignParentNode(tree, None)
+    return
+
+
+def assignParentNode(tree, parentNode):
+    if tree is None:
+        return
+    assignParentNode(tree.left, tree)
+    assignParentNode(tree.right, tree)
+    tree.parent = parentNode
+    return
 
 
 print(findNodesDistanceK(tree=tree, target=3, k=2))
