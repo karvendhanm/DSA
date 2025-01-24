080ad504fce274ef2b607a2b4719944dec481eb0
karvendhanm <karvsmech@gmail.com>
2025-01-24 19:18:30 +0530

    finding target node using recursion

--- find_nodes_distance_k.py
+++ find_nodes_distance_k.py
@@ -1,10 +1,8 @@
 class BT:
-    def __init__(self, value, left=None, right=None, parent=None):
+    def __init__(self, value, left=None, right=None):
         self.value = value
         self.left = left
         self.right = right
-        self.parent = parent
-
 
 
 tree = BT(1)
@@ -23,17 +21,36 @@ tree.right.right.right = BT(8)
 
 def findNodesDistanceK(tree, target, k):
     # Write your code here.
-    assignParentNode(tree, None)
+    parentNodeDetails = {}
+    assignParentNode(tree, None, parentNodeDetails)
+    targetNode = identifyTargetNode(tree, target)
     return
 
 
-def assignParentNode(tree, parentNode):
+def assignParentNode(tree, parentNode, parentNodeDetails):
     if tree is None:
         return
-    assignParentNode(tree.left, tree)
-    assignParentNode(tree.right, tree)
-    tree.parent = parentNode
+    assignParentNode(tree.left, tree, parentNodeDetails)
+    assignParentNode(tree.right, tree, parentNodeDetails)
+    parentNodeDetails[tree] = parentNode
     return
+
+
+def identifyTargetNode(tree, target):
+    if tree is None:
+        return
+
+    if tree.value == target:
+        return tree
+
+    left = identifyTargetNode(tree.left, target)
+    if not left:
+        right = identifyTargetNode(tree.right, target)
+
+    return left or right
+
+
+
 
 
 print(findNodesDistanceK(tree=tree, target=3, k=2))
