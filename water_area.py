c9d61dff542ed55375ab9a2132cc44df1f7c08c3
Karvendhan M <karvsmech@gmail.com>
2025-02-07 14:53:51 +0530

    Update water_area.py adding space and time complexity

--- water_area.py
+++ water_area.py
@@ -1,43 +1,53 @@
-def getBoundingRightWallAtIndex(index, array):
-    boundingRightWallIndex = None
-    boundingRightWallHeight = 0
-    for i in range(index + 1, len(array)):
-        if array[i] > boundingRightWallHeight:
-            boundingRightWallIndex = i
-            boundingRightWallHeight = array[i]
-            if array[i] > array[index]:
+def getBoundingRightWallIndex(index, heights):
+
+    tankRightWallIndex = None
+    tankRightWallHeight = 0
+
+    for i in range(index + 1, len(heights)):
+        if heights[i] > tankRightWallHeight:
+            tankRightWallIndex = i
+            tankRightWallHeight = heights[i]
+            if heights[i] > heights[index]:
                 break
-    return boundingRightWallIndex
+    return tankRightWallIndex
 
 
 def getWaterHoldingCapacity(tank, heights):
+
     leftIndex, rightIndex = tank
-    leftWallHeight = heights[leftIndex]
-    rightWallHeight = heights[rightIndex]
+
+    leftWallHeight, rightWallHeight = heights[leftIndex], heights[rightIndex]
+
     distanceBetweenWalls = rightIndex - leftIndex - 1
     if leftWallHeight > rightWallHeight:
         leftWallHeight, rightWallHeight = rightWallHeight, leftWallHeight
-    intermediateWalls = 0
+
+    intermediateWallsHeight = 0
     for i in range(leftIndex + 1, rightIndex):
-        intermediateWalls += heights[i]
-    return (leftWallHeight * distanceBetweenWalls) - intermediateWalls
+        intermediateWallsHeight += heights[i]
+
+    return (leftWallHeight * distanceBetweenWalls) - intermediateWallsHeight
 
 
 # first iteration
+# O(n ^ 2) time and O(n) space
 def waterArea(heights):
     # Write your code here.
     tanks = []
-    leftIndex = 0
+    leftIndex, totalWaterHoldingCapacity = 0, 0
+
     while leftIndex < len(heights) - 1:
         if heights[leftIndex] == 0:
             leftIndex += 1
             continue
-        rightIndex = getBoundingRightWallAtIndex(leftIndex, heights)
+
+        rightIndex = getBoundingRightWallIndex(leftIndex, heights)
         if rightIndex is None:
             break
+
         tanks.append([leftIndex, rightIndex])
         leftIndex = rightIndex
-    totalWaterHoldingCapacity = 0
+
     for tank in tanks:
         totalWaterHoldingCapacity += getWaterHoldingCapacity(tank, heights)
     return totalWaterHoldingCapacity
