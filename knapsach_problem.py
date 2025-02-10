4cf234d6541fb04053aee3194456386cbbc29b70
Karvendhan M <karvsmech@gmail.com>
2025-02-10 19:58:25 +0530

    Create knapsach_problem.py

---
+++ knapsach_problem.py
@@ -0,0 +1,17 @@
+def knapsackProblem(items, capacity):
+    # Write your code here.
+    # return [
+    #   10, // total value
+    #   [1, 2], // item indices
+    # ]
+    pass
+
+
+items = [
+    [1, 2],
+    [4, 3],
+    [5, 6],
+    [6, 7]
+  ]
+capacity = 10
+knapsackProblem(items, capacity)
