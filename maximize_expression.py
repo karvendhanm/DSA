9f23f51862d2a809ab93840d30c8edfabf927eef
Karvendhan M <karvsmech@gmail.com>
2025-02-20 15:12:44 +0530

    Update maximize_expression.py third iteration cosmetic changes to the second iteration

--- maximize_expression.py
+++ maximize_expression.py
@@ -14,7 +14,35 @@
 #     return maxSum
 
 
-# second iteration - dynamic programming
+# # second iteration - dynamic programming
+# # O(n) time | O(n) space
+# def maximizeExpression(array):
+#     if len(array) < 4:
+#         return 0
+#
+#     max_A = [float("-inf")] * len(array)
+#     max_A_B = [float("-inf")] * len(array)
+#     max_A_B_C = [float("-inf")] * len(array)
+#     max_A_B_C_D = [float("-inf")] * len(array)
+#
+#     max_A[0] = array[0]
+#     for i in range(1, len(array)):
+#         max_A[i] = max(max_A[i-1], array[i])
+#
+#     for i in range(1, len(array)):
+#         max_A_B[i] = max(max_A_B[i-1], max_A[i-1] - array[i])
+#
+#     for i in range(2, len(array)):
+#         max_A_B_C[i] = max(max_A_B_C[i-1], max_A_B[i-1] + array[i])
+#
+#     for i in range(3, len(array)):
+#         max_A_B_C_D[i] = max(max_A_B_C_D[i-1], max_A_B_C[i-1]-array[i])
+#
+#     return max_A_B_C_D[-1]
+
+
+
+# third iteration - cosmetic changes to the second iteration
 # O(n) time | O(n) space
 def maximizeExpression(array):
     if len(array) < 4:
@@ -25,15 +53,14 @@ def maximizeExpression(array):
     max_A_B_C = [float("-inf")] * len(array)
     max_A_B_C_D = [float("-inf")] * len(array)
 
-    # first what is the max value we can have for A if we looked at an index or to the left of the index.
     max_A[0] = array[0]
-    for i in range(1, len(array)):
+    for i in range(1, len(array)-3):
         max_A[i] = max(max_A[i-1], array[i])
 
-    for i in range(1, len(array)):
+    for i in range(1, len(array)-2):
         max_A_B[i] = max(max_A_B[i-1], max_A[i-1] - array[i])
 
-    for i in range(2, len(array)):
+    for i in range(2, len(array)-1):
         max_A_B_C[i] = max(max_A_B_C[i-1], max_A_B[i-1] + array[i])
 
     for i in range(3, len(array)):
