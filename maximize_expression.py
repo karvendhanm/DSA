44d3d2cbdd35ecc66b3aa1752da320968681cbb0
Karvendhan M <karvsmech@gmail.com>
2025-02-20 14:59:16 +0530

    Update maximize_expression.py second iteration O(n) time using dynamic programming

--- maximize_expression.py
+++ maximize_expression.py
@@ -1,17 +1,45 @@
 # first iteration - brute force
 # O(n^4) time | O(1) space
+# def maximizeExpression(array):
+#     # Write your code here.
+#     if len(array) < 4:
+#         return 0
+#
+#     maxSum = float("-inf")
+#     for i in range(len(array)):
+#         for j in range(i+1, len(array)):
+#             for k in range(j+1, len(array)):
+#                 for l in range(k+1, len(array)):
+#                     maxSum = max(maxSum, array[i] - array[j] + array[k] - array[l])
+#     return maxSum
+
+
+# second iteration - dynamic programming
+# O(n) time | O(n) space
 def maximizeExpression(array):
-    # Write your code here.
     if len(array) < 4:
         return 0
 
-    maxSum = float("-inf")
-    for i in range(len(array)):
-        for j in range(i+1, len(array)):
-            for k in range(j+1, len(array)):
-                for l in range(k+1, len(array)):
-                    maxSum = max(maxSum, array[i] - array[j] + array[k] - array[l])
-    return maxSum
+    max_A = [float("-inf")] * len(array)
+    max_A_B = [float("-inf")] * len(array)
+    max_A_B_C = [float("-inf")] * len(array)
+    max_A_B_C_D = [float("-inf")] * len(array)
+
+    # first what is the max value we can have for A if we looked at an index or to the left of the index.
+    max_A[0] = array[0]
+    for i in range(1, len(array)):
+        max_A[i] = max(max_A[i-1], array[i])
+
+    for i in range(1, len(array)):
+        max_A_B[i] = max(max_A_B[i-1], max_A[i-1] - array[i])
+
+    for i in range(2, len(array)):
+        max_A_B_C[i] = max(max_A_B_C[i-1], max_A_B[i-1] + array[i])
+
+    for i in range(3, len(array)):
+        max_A_B_C_D[i] = max(max_A_B_C_D[i-1], max_A_B_C[i-1]-array[i])
+
+    return max_A_B_C_D[-1]
 
 
 array = [3, 6, 1, -3, 2, 7]
