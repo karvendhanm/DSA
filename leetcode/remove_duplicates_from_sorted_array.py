2c15d96e5dda0cf2fd299824a3d31675147a01e2
karvendhanm <karvsmech@gmail.com>
2025-03-10 19:03:02 +0530

    more intuitive solution

--- leetcode/remove_duplicates_from_sorted_array.py
+++ leetcode/remove_duplicates_from_sorted_array.py
@@ -1,3 +1,4 @@
+# first iteration
 # O(n) time | O(1) space
 def removeDuplicates(nums):
     """
@@ -16,6 +17,22 @@ def removeDuplicates(nums):
                 nums[slowPointerIdx] = nums[fastPointerIdx]
         fastPointerIdx += 1
     return slowPointerIdx + 1
+
+
+# second iteration
+# this iteration is more intuitive than the first
+# O(n) time | O(1) space
+def removeDuplicates(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    insertAtIdx = 1
+    for i in range(1, len(nums)):
+        if nums[i - 1] != nums[i]:
+            nums[insertAtIdx] = nums[i]
+            insertAtIdx += 1
+    return insertAtIdx
 
 
 nums = [1, 1, 1, 2, 2, 4, 5, 6, 6, 6, 7]
